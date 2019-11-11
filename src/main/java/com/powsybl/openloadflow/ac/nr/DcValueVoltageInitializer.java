84758cad373b42eb47d75e8d81335d4b2e561d0e
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-11-11 12:13:16 +0100

    Package renaming (#14)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

---
+++ src/main/java/com/powsybl/openloadflow/ac/nr/DcValueVoltageInitializer.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.openloadflow.ac.nr;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.openloadflow.dc.DcLoadFlowEngine;
+import com.powsybl.openloadflow.equations.VoltageInitializer;
+import com.powsybl.openloadflow.network.LfBus;
+import com.powsybl.openloadflow.network.LfNetwork;
+import com.powsybl.math.matrix.MatrixFactory;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+public class DcValueVoltageInitializer implements VoltageInitializer {
+
+    @Override
+    public void prepare(LfNetwork network, MatrixFactory matrixFactory) {
+        if (!new DcLoadFlowEngine(network, matrixFactory).run()) {
+            throw new PowsyblException("DC loadflow failed, impossible to initialize voltage angle from DC values");
+        }
+    }
+
+    @Override
+    public double getMagnitude(LfBus bus) {
+        return 1;
+    }
+
+    @Override
+    public double getAngle(LfBus bus) {
+        return bus.getAngle();
+    }
+}
