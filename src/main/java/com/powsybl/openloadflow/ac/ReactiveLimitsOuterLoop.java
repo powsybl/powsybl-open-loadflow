d2f4ae903c96ccc17e24b6ee637388c67e9b5232
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-03-31 15:19:34 +0200

    Avoid switching all buses PV to PQ (#61)
    * Avoid switching all buses PV to PQ
    * Fix sort stability
    * Fix infinite loop
    * Fix to take into account remote voltage
    
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/ReactiveLimitsOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/ac/ReactiveLimitsOuterLoop.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +30,16 @@ import java.util.stream.Collectors;
 public class ReactiveLimitsOuterLoop implements OuterLoop {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReactiveLimitsOuterLoop.class);
+
+    private static final Comparator<PvToPqBus> BY_NOMINAL_V_COMPARATOR = Comparator.comparingDouble(pvToPqBus -> {
+        LfBus bus = pvToPqBus.bus;
+        LfBus controlledBus = bus.getControlledBus().orElse(null);
+        return controlledBus == null ? -bus.getNominalV() : -controlledBus.getNominalV();
+    });
+
+    private static final Comparator<PvToPqBus> BY_TARGET_P_COMPARATOR = Comparator.comparingDouble(pvToPqBus -> -pvToPqBus.bus.getTargetP());
+
+    private static final Comparator<PvToPqBus> BY_ID_COMPARATOR = Comparator.comparing(pvToPqBus -> pvToPqBus.bus.getId());
 
     @Override
     public String getName() {
@@ -63,35 +74,88 @@ public class ReactiveLimitsOuterLoop implements OuterLoop {
         }
     }
 
+    private enum ReactiveLimitDirection {
+        MIN,
+        MAX
+    }
+
+    private static final class PvToPqBus {
+
+        private final LfBus bus;
+
+        private final double q;
+
+        private final double qLimit;
+
+        private final ReactiveLimitDirection limitDirection;
+
+        private PvToPqBus(LfBus bus, double q, double qLimit, ReactiveLimitDirection limitDirection) {
+            this.bus = bus;
+            this.q = q;
+            this.qLimit = qLimit;
+            this.limitDirection = limitDirection;
+        }
+    }
+
+    private OuterLoopStatus switchPvPq(List<PvToPqBus> pvToPqBuses, EquationSystem equationSystem, VariableSet variableSet, int remainingPvBusCount) {
+        OuterLoopStatus status = OuterLoopStatus.STABLE;
+
+        int modifiedRemainingPvBusCount = remainingPvBusCount;
+        if (modifiedRemainingPvBusCount == 0) {
+            // keep one bus PV, the strongest one which is one at the highest nominal level and highest active power
+            // target
+            PvToPqBus strongestPvToPqBus = pvToPqBuses.stream()
+                    .min(BY_NOMINAL_V_COMPARATOR
+                            .thenComparing(BY_TARGET_P_COMPARATOR)
+                            .thenComparing(BY_ID_COMPARATOR)) // for stability of the sort
+                    .orElseThrow(IllegalStateException::new);
+            pvToPqBuses.remove(strongestPvToPqBus);
+            modifiedRemainingPvBusCount++;
+            LOGGER.warn("All PV buses should switch PQ, strongest one '{}' will stay PV", strongestPvToPqBus.bus.getId());
+        }
+
+        if (!pvToPqBuses.isEmpty()) {
+            status = OuterLoopStatus.UNSTABLE;
+
+            for (PvToPqBus pvToPqBus : pvToPqBuses) {
+                // switch PV -> PQ
+                switchPvPq(pvToPqBus.bus, equationSystem, variableSet, pvToPqBus.qLimit);
+                LOGGER.trace("Switch bus {} PV -> PQ, q={} < {}Q={}", pvToPqBus.bus.getId(), pvToPqBus.q * PerUnit.SB,
+                        pvToPqBus.limitDirection == ReactiveLimitDirection.MAX ? "max" : "min", pvToPqBus.qLimit * PerUnit.SB);
+            }
+        }
+
+        LOGGER.info("{} buses switched PV -> PQ ({} bus remains PV}", pvToPqBuses.size(), modifiedRemainingPvBusCount);
+
+        return status;
+    }
+
     @Override
     public OuterLoopStatus check(OuterLoopContext context) {
-        OuterLoopStatus status = OuterLoopStatus.STABLE;
-        List<LfBus> pvToPqBuses = new ArrayList<>();
+        List<PvToPqBus> pvToPqBuses = new ArrayList<>();
+        int remainingPvBusCount = 0;
         for (LfBus bus : context.getNetwork().getBuses()) {
             if (bus.hasVoltageControl()) { // PV bus
                 double q = bus.getCalculatedQ() + bus.getLoadTargetQ();
                 double minQ = bus.getMinQ();
                 double maxQ = bus.getMaxQ();
                 if (q < minQ) {
-                    // switch PV -> PQ
-                    switchPvPq(bus, context.getEquationSystem(), context.getVariableSet(), minQ);
-                    LOGGER.trace("Switch bus {} PV -> PQ, q={} < minQ={}", bus.getId(), q * PerUnit.SB, minQ * PerUnit.SB);
-                    pvToPqBuses.add(bus);
-                    status = OuterLoopStatus.UNSTABLE;
+                    pvToPqBuses.add(new PvToPqBus(bus, q, minQ, ReactiveLimitDirection.MIN));
                 } else if (q > maxQ) {
                     // switch PV -> PQ
-                    switchPvPq(bus, context.getEquationSystem(), context.getVariableSet(), maxQ);
-                    LOGGER.trace("Switch bus {} PV -> PQ, q={} MVar > maxQ={}", bus.getId(), q * PerUnit.SB, maxQ * PerUnit.SB);
-                    pvToPqBuses.add(bus);
-                    status = OuterLoopStatus.UNSTABLE;
+                    pvToPqBuses.add(new PvToPqBus(bus, q, maxQ, ReactiveLimitDirection.MAX));
+                } else {
+                    remainingPvBusCount++;
                 }
             } else { // PQ bus
                 // TODO
             }
         }
+
         if (!pvToPqBuses.isEmpty()) {
-            LOGGER.info("{} buses switched PV -> PQ", pvToPqBuses.size());
+            return switchPvPq(pvToPqBuses, context.getEquationSystem(), context.getVariableSet(), remainingPvBusCount);
         }
-        return status;
+
+        return OuterLoopStatus.STABLE;
     }
 }
