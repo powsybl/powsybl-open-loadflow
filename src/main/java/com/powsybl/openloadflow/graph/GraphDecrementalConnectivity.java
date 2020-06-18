d12ec98a188fa3da734e1bcf7a000a9a12f15b3d
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-06-18 14:20:06 +0200

    Add GraphDecrementalConnectivity
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ src/main/java/com/powsybl/openloadflow/graph/GraphDecrementalConnectivity.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.openloadflow.graph;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+public interface GraphDecrementalConnectivity<V> {
+
+    void addVertex(V vertex);
+
+    void addEdge(V vertex1, V vertex2);
+
+    void cut(V vertex1, V vertex2);
+
+    void reset();
+
+    boolean isConnected(V vertex1, V vertex2);
+}
