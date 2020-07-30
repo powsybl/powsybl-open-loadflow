ae6ba27ce9239761c8dd7d92404f02dbdbede0bd
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-07-30 11:28:08 +0200

    Add unit tests
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/network/NetworkSlackBusSelectorTest.java
+++ src/test/java/com/powsybl/openloadflow/network/NetworkSlackBusSelectorTest.java
@@ -6,29 +6,72 @@
  */
 package com.powsybl.openloadflow.network;
 
+import com.powsybl.iidm.network.Generator;
 import com.powsybl.iidm.network.Load;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.iidm.network.extensions.SlackTerminalAdder;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Anne Tilloy <anne.tilloy at rte-france.com>
  */
 class NetworkSlackBusSelectorTest {
 
+    private Network network;
+
+    private SlackBusSelector selectorMock;
+
+    private boolean[] fallbackUsed;
+
+    @BeforeEach
+    void setUp() {
+        network = EurostagTutorialExample1Factory.create();
+        fallbackUsed = new boolean[1];
+        MostMeshedSlackBusSelector selectorFallback = new MostMeshedSlackBusSelector();
+        selectorMock = buses -> {
+            fallbackUsed[0] = true;
+            return selectorFallback.select(buses);
+        };
+    }
+
     @Test
-    void test() {
-        Network network = EurostagTutorialExample1Factory.create();
+    void noExtensionTest() {
+        LfNetwork lfNetwork = LfNetwork.load(network, new NetworkSlackBusSelector(network, selectorMock)).get(0);
+        assertEquals("VLHV1_0", lfNetwork.getSlackBus().getId());
+        assertTrue(fallbackUsed[0]);
+    }
+
+    @Test
+    void oneExtensionTest() {
         VoltageLevel vlload = network.getVoltageLevel("VLLOAD");
         Load load = network.getLoad("LOAD");
         vlload.newExtension(SlackTerminalAdder.class)
                 .withTerminal(load.getTerminal())
                 .add();
-        LfNetwork lfNetwork = LfNetwork.load(network, new NetworkSlackBusSelector(network)).get(0);
+        LfNetwork lfNetwork = LfNetwork.load(network, new NetworkSlackBusSelector(network, selectorMock)).get(0);
         assertEquals("VLLOAD_0", lfNetwork.getSlackBus().getId());
+        assertFalse(fallbackUsed[0]);
+    }
+
+    @Test
+    void twoExtensionsTest() {
+        VoltageLevel vlload = network.getVoltageLevel("VLLOAD");
+        VoltageLevel vlgen = network.getVoltageLevel("VLGEN");
+        Load load = network.getLoad("LOAD");
+        Generator gen = network.getGenerator("GEN");
+        vlload.newExtension(SlackTerminalAdder.class)
+                .withTerminal(load.getTerminal())
+                .add();
+        vlgen.newExtension(SlackTerminalAdder.class)
+                .withTerminal(gen.getTerminal())
+                .add();
+        LfNetwork lfNetwork = LfNetwork.load(network, new NetworkSlackBusSelector(network, selectorMock)).get(0);
+        assertEquals("VLLOAD_0", lfNetwork.getSlackBus().getId());
+        assertTrue(fallbackUsed[0]);
     }
 }
