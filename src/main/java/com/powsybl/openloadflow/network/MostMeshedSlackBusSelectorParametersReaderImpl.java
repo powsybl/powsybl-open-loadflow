f0339c3cd0e96f3d0862b56523711bbb13eb62c2
Thomas ADAM <tadam@silicom.fr>
2020-09-25 09:49:39 +0200

    Move MostMeshedSlackBusSelectorParametersReaderImpl into network folder
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

--- src/main/java/com/powsybl/openloadflow/network/MostMeshedSlackBusSelectorParametersReaderImpl.java
+++ src/main/java/com/powsybl/openloadflow/network/MostMeshedSlackBusSelectorParametersReaderImpl.java
@@ -4,13 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.openloadflow.network.impl;
+package com.powsybl.openloadflow.network;
 
 import com.google.auto.service.AutoService;
 import com.powsybl.commons.config.ModuleConfig;
-import com.powsybl.openloadflow.network.MostMeshedSlackBusSelector;
-import com.powsybl.openloadflow.network.SlackBusSelector;
-import com.powsybl.openloadflow.network.SlackBusSelectorParametersReader;
 
 /**
  * @author Thomas Adam <tadam at silicom.fr>
