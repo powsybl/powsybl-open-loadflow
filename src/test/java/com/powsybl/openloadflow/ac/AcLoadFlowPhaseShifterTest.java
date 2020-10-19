e433917a464c55bc99f26dc08ddd68ddd38eca7b
Florian Dupuy <florian.dupuy@rte-france.com>
2020-10-19 20:16:25 +0200

    Add unit test
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowPhaseShifterTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowPhaseShifterTest.java
@@ -134,14 +134,25 @@ class AcLoadFlowPhaseShifterTest {
     void remoteFlowControlT2wtTest() {
         selectNetwork(createNetworkWithT2wt());
         parameters.setPhaseShifterRegulationOn(true);
-        t2wt.getPhaseTapChanger().setRegulationMode(PhaseTapChanger.RegulationMode.ACTIVE_POWER_CONTROL)
-                .setTargetDeadband(1) // FIXME how to take this into account
+        t2wt.getPhaseTapChanger().setRegulationMode(PhaseTapChanger.RegulationMode.CURRENT_LIMITER)
+                .setTargetDeadband(10)
                 .setRegulating(true)
                 .setTapPosition(2)
                 .setRegulationTerminal(line1.getTerminal1())
                 .setRegulationValue(83);
 
         LoadFlowResult result = loadFlowRunner.run(network, parameters);
+        assertTrue(result.isOk());
+        assertActivePowerEquals(16.541, line1.getTerminal1());
+        assertActivePowerEquals(83.587, line2.getTerminal1());
+        assertEquals(2, t2wt.getPhaseTapChanger().getTapPosition());
+
+        // Test with narrow target deadband
+        t2wt.getPhaseTapChanger()
+            .setRegulationMode(PhaseTapChanger.RegulationMode.ACTIVE_POWER_CONTROL)
+            .setTargetDeadband(1); // FIXME how to take this into account
+
+        result = loadFlowRunner.run(network, parameters);
         assertTrue(result.isOk());
         assertActivePowerEquals(83.688, line1.getTerminal1());
         assertActivePowerEquals(16.527, line2.getTerminal1());
