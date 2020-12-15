515be223dd08575af9460f557cf4623b638c4f96
Gael Macherel <gael.macherel@artelys.com>
2020-12-15 09:49:05 +0100

    fix code smell
    Signed-off-by: Gael Macherel <gael.macherel@artelys.com>

--- src/test/java/com/powsybl/openloadflow/sensi/DcSensitivityAnalysisTest.java
+++ src/test/java/com/powsybl/openloadflow/sensi/DcSensitivityAnalysisTest.java
@@ -292,7 +292,7 @@ class DcSensitivityAnalysisTest extends AbstractSensitivityAnalysisTest {
         CompletionException exception = assertThrows(CompletionException.class, () -> sensiProvider.run(network, VariantManagerConstants.INITIAL_VARIANT_ID,
                 factorsProvider, new EmptyContingencyListProvider(), sensiParameters, LocalComputationManager.getDefault()).join());
         assertTrue(exception.getCause() instanceof PowsyblException);
-        assertEquals(exception.getCause().getMessage(), "Cannot analyse sensitivity of slack bus");
+        assertEquals("Cannot analyse sensitivity of slack bus", exception.getCause().getMessage());
     }
 
     @Test
@@ -307,7 +307,7 @@ class DcSensitivityAnalysisTest extends AbstractSensitivityAnalysisTest {
         CompletionException exception = assertThrows(CompletionException.class, () -> sensiProvider.run(network, VariantManagerConstants.INITIAL_VARIANT_ID,
                 factorsProvider, new EmptyContingencyListProvider(), sensiParameters, LocalComputationManager.getDefault()).join());
         assertTrue(exception.getCause() instanceof UnsupportedOperationException);
-        assertEquals(exception.getCause().getMessage(), "Balance type not yet supported: PROPORTIONAL_TO_CONFORM_LOAD");
+        assertEquals("Balance type not yet supported: PROPORTIONAL_TO_CONFORM_LOAD", exception.getCause().getMessage());
     }
 
     @Test
