d83b92c40bfa641625a98ca53ec7cda720936298
Gael Macherel <gael.macherel@artelys.com>
2021-03-11 17:41:58 +0100

    remove variables from BranchState
    Signed-off-by: Gael Macherel <gael.macherel@artelys.com>

--- src/main/java/com/powsybl/openloadflow/util/BranchState.java
+++ src/main/java/com/powsybl/openloadflow/util/BranchState.java
@@ -10,33 +10,17 @@ import java.util.stream.Collectors;
 public class BranchState {
     private final double a1;
     private final double r1;
-    private final double x;
-    private final double r;
-    private final DiscretePhaseControl phaseControl;
-    private final DiscreteVoltageControl voltageControl;
 
     public BranchState(LfBranch b) {
         PiModel piModel = b.getPiModel();
         a1 = piModel.getA1();
         r1 = piModel.getR1();
-        x = piModel.getX();
-        r = piModel.getR();
-        phaseControl = b.getDiscretePhaseControl();
-        voltageControl = b.getDiscreteVoltageControl();
-    }
-
-    public void restoreBranchState(LfBranch branch) {
-        restoreBranchActiveState(branch);
-        branch.setDiscretePhaseControl(phaseControl);
-        branch.setDiscreteVoltageControl(voltageControl);
     }
 
     public void restoreBranchActiveState(LfBranch branch) {
         PiModel piModel = branch.getPiModel();
         piModel.setA1(a1);
         piModel.setR1(r1);
-        piModel.setR(r);
-        piModel.setX(x);
     }
 
     /**
@@ -46,14 +30,6 @@ public class BranchState {
      */
     public static Map<LfBranch, BranchState> createBranchStates(Collection<LfBranch> branches) {
         return branches.stream().collect(Collectors.toMap(Function.identity(), BranchState::new));
-    }
-
-    /**
-     * Set the branch states based on the given map of states
-     * @param branchStates the map containing the branches states, indexed by branches
-     */
-    public static void restoreBranchStates(Map<LfBranch, BranchState> branchStates) {
-        branchStates.forEach((b, state) -> state.restoreBranchState(b));
     }
 
     /**
