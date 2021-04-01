cf910e447350d4bca1eff5bda481dd7709ed01c6
Anne Tilloy <anne.tilloy@rte-france.com>
2021-04-01 11:01:45 +0200

    Fixes.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/graph/ConflictingVoltageControlManager.java
+++ src/main/java/com/powsybl/openloadflow/graph/ConflictingVoltageControlManager.java
@@ -2,7 +2,6 @@ package com.powsybl.openloadflow.graph;
 
 import com.powsybl.openloadflow.network.*;
 import com.powsybl.openloadflow.network.impl.LfLegBranch;
-import net.jafama.FastMath;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jgrapht.Graph;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -20,16 +19,10 @@ public class ConflictingVoltageControlManager {
     static class VoltageLevelIndex {
         private double minV;
         private double maxV;
-        private double qmax;
 
-        public VoltageLevelIndex(double minV, double maxV, double qmax) {
+        public VoltageLevelIndex(double minV, double maxV) {
             this.minV = minV;
             this.maxV = maxV;
-            this.qmax = qmax;
-        }
-
-        public double getQmax() {
-            return qmax;
         }
 
         public boolean match(LfBus lfBus) {
@@ -39,16 +32,18 @@ public class ConflictingVoltageControlManager {
 
     static Collection<VoltageLevelIndex> createVoltageLevels() {
         List<VoltageLevelIndex> indexes = new ArrayList<>();
-        indexes.add(new VoltageLevelIndex(Double.MIN_VALUE, 50, 400d));
-        indexes.add(new VoltageLevelIndex(50, 80, 400d));
-        indexes.add(new VoltageLevelIndex(80, 150, 400d));
-        indexes.add(new VoltageLevelIndex(150, 300, 880d));
-        indexes.add(new VoltageLevelIndex(300, 500, 1600d));
-        indexes.add(new VoltageLevelIndex(500, Double.MAX_VALUE, 1600d));
+        indexes.add(new VoltageLevelIndex(Double.MIN_VALUE, 50));
+        indexes.add(new VoltageLevelIndex(50, 80));
+        indexes.add(new VoltageLevelIndex(80, 150));
+        indexes.add(new VoltageLevelIndex(150, 300));
+        indexes.add(new VoltageLevelIndex(300, 500));
+        indexes.add(new VoltageLevelIndex(500, Double.MAX_VALUE));
         return indexes;
     }
 
     LfNetwork lfNetwork;
+    private Graph<LfBus, LfBranch> graph = new DefaultUndirectedWeightedGraph<>(LfBranch.class);
+    private DijkstraShortestPath<LfBus, LfBranch> algorithm;
     private final Collection<VoltageLevelIndex> voltageLevelIndices = createVoltageLevels();
     private final Set<LfBus> pvBuses;
 
@@ -58,6 +53,17 @@ public class ConflictingVoltageControlManager {
      * @param lfBuses needed, because calling lfNetwork.getBuses() would trigger updateCache and crash in slackBus selection for non-principal components
      */
     public ConflictingVoltageControlManager(LfNetwork lfNetwork, List<LfBus> lfBuses) {
+        lfBuses.forEach(bus -> graph.addVertex(bus));
+        lfNetwork.getBranches().stream()
+            .filter(branch -> branch.getBus1() != null && branch.getBus2() != null)
+            .filter(branch -> !(branch.getPiModel() instanceof PiModelArray)) // for t2wt
+            .filter(branch -> !(branch instanceof LfLegBranch)) // for t3wt
+            .forEach(branch -> {
+                graph.addEdge(branch.getBus1(), branch.getBus2(), branch);
+                graph.setEdgeWeight(branch.getBus1(), branch.getBus2(),
+                        Math.abs(branch.getPiModel().getX()) * (branch.getBus2().getNominalV() * branch.getBus2().getNominalV()) / PerUnit.SB);
+            });
+        this.algorithm = new DijkstraShortestPath<>(graph);
         this.lfNetwork = lfNetwork;
         this.pvBuses = getPvBuses(lfBuses);
     }
@@ -77,29 +83,6 @@ public class ConflictingVoltageControlManager {
             .collect(Collectors.toSet());
     }
 
-    private static Graph<LfBus, LfBranch> createGraph(Collection<LfBus> buses) {
-        Graph<LfBus, LfBranch> graph = new DefaultUndirectedWeightedGraph<>(LfBranch.class);
-
-        // first, add all buses
-        for (LfBus bus : buses) {
-            graph.addVertex(bus);
-        }
-        // then, add all branches
-        for (LfBus bus : buses) {
-            bus.getBranches().stream()
-                .filter(branch -> branch.getBus1() != null && branch.getBus2() != null)
-                .filter(branch -> buses.contains(branch.getBus1()) && buses.contains(branch.getBus2()))
-                .filter(branch -> !branch.isVoltageController()) // twt ?
-                .filter(branch -> !(branch instanceof LfLegBranch)) // T3wt are LegBranches ?
-                .forEach(branch -> {
-                    graph.addEdge(branch.getBus1(), branch.getBus2(), branch);
-                    graph.setEdgeWeight(branch.getBus1(), branch.getBus2(), FastMath.abs(branch.getPiModel().getX()));
-                });
-        }
-
-        return graph;
-    }
-
     /***
      *
      * @return Collection of pairs of lfbuses that have different voltage levels, the first element of the pair being the highest voltage
@@ -108,12 +91,12 @@ public class ConflictingVoltageControlManager {
         Collection<Pair<LfBus, LfBus>> pairs = new ArrayList<>();
         for (int i = 0; i < buses.size() - 1; i++) {
             for (int j = i + 1; j < buses.size(); j++) {
-                LfBus busi = buses.get(i);
-                LfBus busj = buses.get(j);
-                if (busi.getV() * busi.getNominalV() - busj.getV() * busj.getNominalV() > 0) {
-                    pairs.add(Pair.of(busi, busj));
-                } else if (busi.getV() * busi.getNominalV() - busj.getV() * busj.getNominalV() < 0) {
-                    pairs.add(Pair.of(busj, busi));
+                LfBus iBus = buses.get(i);
+                LfBus jBus = buses.get(j);
+                if (iBus.getV() * iBus.getNominalV() - jBus.getV() * jBus.getNominalV() > 0) {
+                    pairs.add(Pair.of(iBus, jBus));
+                } else if (iBus.getV() * iBus.getNominalV() - jBus.getV() * jBus.getNominalV() < 0) {
+                    pairs.add(Pair.of(jBus, iBus));
                 }
             }
         }
@@ -127,10 +110,7 @@ public class ConflictingVoltageControlManager {
         for (Map.Entry<VoltageLevelIndex, Set<LfBus>> entry : busesByVoltageLevel.entrySet()) {
             VoltageLevelIndex index = entry.getKey();
             Set<LfBus> buses = entry.getValue();
-            Graph<LfBus, LfBranch> voltageLevelGraph = createGraph(buses);
-            DijkstraShortestPath<LfBus, LfBranch> algorithm = new DijkstraShortestPath<>(voltageLevelGraph);
             Collection<Pair<LfBus, LfBus>> conflicts = new ArrayList<>();
-
             conflictsByVoltageLevel.put(index, conflicts);
 
             if (buses.size() < 2) {
@@ -138,16 +118,28 @@ public class ConflictingVoltageControlManager {
             }
             Collection<Pair<LfBus, LfBus>> candidatesPvPairs = getCandidatePairs(List.copyOf(buses));
 
+            // todo: it may be better to compute the paths ourselves (to stop at transformer, or stop if weight > threshold)
             for (Pair<LfBus, LfBus> candidatePair : candidatesPvPairs) {
                 LfBus bus1 = candidatePair.getKey();
                 LfBus bus2 = candidatePair.getValue();
-                double pathWeight = algorithm.getPathWeight(bus1, bus2); // todo: it may be better to compute the paths ourselves (to stop at transformer, or stop if weight > threshold)
-
-                double conflictThreshold = bus1.getV() * bus1.getNominalV() * (bus1.getV() * bus1.getNominalV() - bus2.getV() * bus2.getNominalV()) / index.getQmax();
+                double pathWeight = algorithm.getPathWeight(bus1, bus2);
+                double targetV1;
+                double targetV2;
+                if (bus1.isVoltageControlled()) {
+                    targetV1 = bus1.getVoltageControl().get().getTargetValue() * bus1.getNominalV();
+                } else {
+                    targetV1 = bus1.getDiscreteVoltageControl().getTargetValue() * bus1.getNominalV();
+                }
+                if (bus2.isVoltageControlled()) {
+                    targetV2 = bus2.getVoltageControl().get().getTargetValue() * bus2.getNominalV();
+                } else {
+                    targetV2 = bus2.getDiscreteVoltageControl().getTargetValue() * bus2.getNominalV();
+                }
+                double conflictThreshold = Math.abs(targetV1 - targetV2) * 1.5;
                 if (pathWeight < conflictThreshold) {
                     conflicts.add(candidatePair);
-                    LOGGER.debug("Conflict found between buses: {} and {}. Impedance is {} and voltage difference is {}",
-                        candidatePair.getLeft().getId(), candidatePair.getRight().getId(), pathWeight, conflictThreshold);
+                    LOGGER.debug("Conflict found between regulated buses: {} and {}. Impedance between buses is around {} Ohm and voltage difference is {} kV",
+                        candidatePair.getLeft().getId(), candidatePair.getRight().getId(), pathWeight, Math.abs(targetV1 - targetV2));
                 }
             }
 
@@ -177,7 +169,6 @@ public class ConflictingVoltageControlManager {
 
         for (LfBus bus : busesToSwitch) {
             if (bus.isVoltageControlled()) {
-                LOGGER.debug("Voltage control of bus {} has a conflict but cannot be disabled because it comes from a generator.", bus.getId());
                 continue; // this means we have a conflict between two generators, we do not want to deactivate them
             }
             DiscreteVoltageControl discreteVoltageControl = bus.getDiscreteVoltageControl();
