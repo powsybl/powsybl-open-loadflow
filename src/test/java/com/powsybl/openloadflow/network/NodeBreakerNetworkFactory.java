f7c4774c5ac6652db5906199bd29f27b84a5d914
piloquetcol <coline.piloquet@rte-france.com>
2021-04-28 10:06:02 +0200

    Fix licences
    Signed-off-by: piloquetcol <coline.piloquet@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/network/NodeBreakerNetworkFactory.java
+++ src/test/java/com/powsybl/openloadflow/network/NodeBreakerNetworkFactory.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openloadflow.network;
 
 import com.powsybl.iidm.network.Network;
@@ -39,6 +45,8 @@ public class NodeBreakerNetworkFactory extends AbstractLoadFlowNetworkFactory {
      *      ---o---
      *         |
      *         LD
+     *
+     * @author Gael Macharel <gael.macherel at artelys.com>
      */
     public static Network create() {
         Network network = Network.create("test", "test");
