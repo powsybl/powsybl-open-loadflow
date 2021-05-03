2e50513a80947baee08e347ff98b444eba1998dd
Anne Tilloy <anne.tilloy@rte-france.com>
2021-05-03 14:33:57 +0200

    Create a factory for dangling lines.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowDanglingLineTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowDanglingLineTest.java
@@ -13,6 +13,7 @@ import com.powsybl.loadflow.LoadFlowResult;
 import com.powsybl.math.matrix.DenseMatrixFactory;
 import com.powsybl.openloadflow.OpenLoadFlowParameters;
 import com.powsybl.openloadflow.OpenLoadFlowProvider;
+import com.powsybl.openloadflow.network.DanglingLineFactory;
 import com.powsybl.openloadflow.network.SlackBusSelectionMode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,76 +38,13 @@ class AcLoadFlowDanglingLineTest {
 
     private OpenLoadFlowParameters parametersExt;
 
-    private Network createNetwork() {
-        Network network = Network.create("dl", "test");
-        Substation s1 = network.newSubstation()
-                .setId("S1")
-                .add();
-        Substation s2 = network.newSubstation()
-                .setId("S2")
-                .add();
-        VoltageLevel vl1 = s1.newVoltageLevel()
-                .setId("vl1")
-                .setNominalV(400)
-                .setTopologyKind(TopologyKind.BUS_BREAKER)
-                .add();
-        bus1 = vl1.getBusBreakerView().newBus()
-                .setId("b1")
-                .add();
-        g1 = vl1.newGenerator()
-                .setId("g1")
-                .setConnectableBus("b1")
-                .setBus("b1")
-                .setTargetP(101.3664)
-                .setTargetV(390)
-                .setMinP(0)
-                .setMaxP(150)
-                .setVoltageRegulatorOn(true)
-                .add();
-        VoltageLevel vl2 = s2.newVoltageLevel()
-                .setId("vl2")
-                .setNominalV(400)
-                .setTopologyKind(TopologyKind.BUS_BREAKER)
-                .add();
-        bus2 = vl2.getBusBreakerView().newBus()
-                .setId("b2")
-                .add();
-        dl1 = vl2.newDanglingLine()
-                .setId("dl1")
-                .setConnectableBus("b2")
-                .setBus("b2")
-                .setR(0.7)
-                .setX(1)
-                .setG(Math.pow(10, -6))
-                .setB(3 * Math.pow(10, -6))
-                .setP0(101)
-                .setQ0(150)
-                .newGeneration()
-                    .setTargetP(0)
-                    .setTargetQ(0)
-                    .setTargetV(390)
-                    .setVoltageRegulationOn(false)
-                    .add()
-                .add();
-        network.newLine()
-                .setId("l1")
-                .setVoltageLevel1("vl1")
-                .setBus1("b1")
-                .setVoltageLevel2("vl2")
-                .setBus2("b2")
-                .setR(1)
-                .setX(3)
-                .setG1(0)
-                .setG2(0)
-                .setB1(0)
-                .setB2(0)
-                .add();
-        return network;
-    }
-
     @BeforeEach
     void setUp() {
-        network = createNetwork();
+        network = DanglingLineFactory.create();
+        bus1 = network.getBusBreakerView().getBus("b1");
+        bus2 = network.getBusBreakerView().getBus("b2");
+        dl1 = network.getDanglingLine("dl1");
+        g1 = network.getGenerator("g1");
         loadFlowRunner = new LoadFlow.Runner(new OpenLoadFlowProvider(new DenseMatrixFactory()));
         parameters = new LoadFlowParameters().setNoGeneratorReactiveLimits(true)
                 .setDistributedSlack(false);
---
+++ src/test/java/com/powsybl/openloadflow/network/DanglingLineFactory.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.openloadflow.network;
+
+import com.powsybl.iidm.network.*;
+
+/**
+ * @author Anne Tilloy <anne.tilloy at artelys.com>
+ */
+public class DanglingLineFactory extends AbstractLoadFlowNetworkFactory {
+
+    public static Network create() {
+        Network network = Network.create("dl", "test");
+        Substation s1 = network.newSubstation()
+                .setId("S1")
+                .add();
+        Substation s2 = network.newSubstation()
+                .setId("S2")
+                .add();
+        VoltageLevel vl1 = s1.newVoltageLevel()
+                .setId("vl1")
+                .setNominalV(400)
+                .setTopologyKind(TopologyKind.BUS_BREAKER)
+                .add();
+        vl1.getBusBreakerView().newBus()
+                .setId("b1")
+                .add();
+        vl1.newGenerator()
+                .setId("g1")
+                .setConnectableBus("b1")
+                .setBus("b1")
+                .setTargetP(101.3664)
+                .setTargetV(390)
+                .setMinP(0)
+                .setMaxP(150)
+                .setVoltageRegulatorOn(true)
+                .add();
+        VoltageLevel vl2 = s2.newVoltageLevel()
+                .setId("vl2")
+                .setNominalV(400)
+                .setTopologyKind(TopologyKind.BUS_BREAKER)
+                .add();
+        vl2.getBusBreakerView().newBus()
+                .setId("b2")
+                .add();
+        vl2.newDanglingLine()
+                .setId("dl1")
+                .setConnectableBus("b2")
+                .setBus("b2")
+                .setR(0.7)
+                .setX(1)
+                .setG(Math.pow(10, -6))
+                .setB(3 * Math.pow(10, -6))
+                .setP0(101)
+                .setQ0(150)
+                .newGeneration()
+                .setTargetP(0)
+                .setTargetQ(0)
+                .setTargetV(390)
+                .setVoltageRegulationOn(false)
+                .add()
+                .add();
+        network.newLine()
+                .setId("l1")
+                .setVoltageLevel1("vl1")
+                .setBus1("b1")
+                .setVoltageLevel2("vl2")
+                .setBus2("b2")
+                .setR(1)
+                .setX(3)
+                .setG1(0)
+                .setG2(0)
+                .setB1(0)
+                .setB2(0)
+                .add();
+        return network;
+    }
+
+}
