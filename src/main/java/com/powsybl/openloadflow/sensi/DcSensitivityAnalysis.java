8696e0f8890e9321d070c5db96e19cb638440758
Anne Tilloy <anne.tilloy@rte-france.com>
2021-12-01 11:32:43 +0100

    Fix CI.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/sensi/DcSensitivityAnalysis.java
+++ src/main/java/com/powsybl/openloadflow/sensi/DcSensitivityAnalysis.java
@@ -688,8 +688,7 @@ public class DcSensitivityAnalysis extends AbstractSensitivityAnalysis<DcVariabl
                             .filter(participatingElement -> !participatingGeneratorsToRemove.contains(participatingElement.getElement()))
                             .map(participatingElement -> new ParticipatingElement(participatingElement.getElement(), participatingElement.getFactor()))
                             .collect(Collectors.toList());
-                    String elementType = isDistributedSlackOnLoads(lfParameters) ? "LfBus" : "LfGenerators";
-                    normalizeParticipationFactors(newParticipatingElements, elementType);
+                    normalizeParticipationFactors(newParticipatingElements, "LfGenerators");
                 } else { // slack distribution on loads
                     newParticipatingElements = getParticipatingElements(lfNetwork.getBuses(), lfParameters, lfParametersExt);
                 }
