e48ec3fa48c2b6f66bf526d3fa99efb2950fa589
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2021-12-05 12:47:33 +0100

    Add unit tests
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/test/java/com/powsybl/openloadflow/ac/VoltageMagnitudeInitializerTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/VoltageMagnitudeInitializerTest.java
@@ -7,51 +7,64 @@
 package com.powsybl.openloadflow.ac;
 
 import com.powsybl.ieeecdf.converter.IeeeCdfNetworkFactory;
-import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
-import com.powsybl.loadflow.LoadFlow;
-import com.powsybl.loadflow.LoadFlowParameters;
-import com.powsybl.loadflow.LoadFlowResult;
 import com.powsybl.math.matrix.DenseMatrixFactory;
-import com.powsybl.openloadflow.OpenLoadFlowParameters;
-import com.powsybl.openloadflow.OpenLoadFlowProvider;
-import com.powsybl.openloadflow.network.SlackBusSelectionMode;
-import org.junit.jupiter.api.BeforeEach;
+import com.powsybl.openloadflow.network.FirstSlackBusSelector;
+import com.powsybl.openloadflow.network.LfBus;
+import com.powsybl.openloadflow.network.LfNetwork;
+import com.powsybl.openloadflow.network.impl.LfNetworkLoaderImpl;
+import com.powsybl.openloadflow.network.util.VoltageInitializer;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
-class VoltageMagnitudeInitializerTest {
+public class VoltageMagnitudeInitializerTest {
 
-    private LoadFlow.Runner loadFlowRunner;
-    private LoadFlowParameters parameters;
-    private OpenLoadFlowParameters parametersExt;
-
-    @BeforeEach
-    void setUp() {
-        loadFlowRunner = new LoadFlow.Runner(new OpenLoadFlowProvider(new DenseMatrixFactory()));
-        parameters = new LoadFlowParameters().setNoGeneratorReactiveLimits(true)
-                .setDistributedSlack(false);
-        parametersExt = new OpenLoadFlowParameters()
-                .setVoltageInitMode(OpenLoadFlowParameters.VoltageInitMode.VOLTAGE_MAGNITUDE)
-                .setSlackBusSelectionMode(SlackBusSelectionMode.FIRST);
-        parameters.addExtension(OpenLoadFlowParameters.class, parametersExt);
+    public static void assertBusVoltage(LfNetwork network, VoltageInitializer initializer, String busId, double vRef, double angleRef) {
+        LfBus bus = network.getBusById(busId);
+        double v = initializer.getMagnitude(bus);
+        double angle = initializer.getAngle(bus);
+        assertNotNull(bus);
+        assertEquals(vRef, v, 1E-4d);
+        assertEquals(angleRef, angle, 1E-2d);
     }
 
     @Test
     void testEsgTuto1() {
         Network network = EurostagTutorialExample1Factory.create();
-        LoadFlowResult result = loadFlowRunner.run(network, parameters);
-        assertTrue(result.isOk());
+        LfNetwork lfNetwork = LfNetwork.load(network, new LfNetworkLoaderImpl(), new FirstSlackBusSelector()).get(0);
+        VoltageMagnitudeInitializer initializer = new VoltageMagnitudeInitializer(new DenseMatrixFactory());
+        initializer.prepare(lfNetwork);
+        assertBusVoltage(lfNetwork, initializer, "VLGEN_0", 1.0208, 0);
+        assertBusVoltage(lfNetwork, initializer, "VLHV1_0", 1.0745, 0);
+        assertBusVoltage(lfNetwork, initializer, "VLHV2_0", 1.0745, 0);
+        assertBusVoltage(lfNetwork, initializer, "VLLOAD_0", 1.076, 0);
     }
 
     @Test
     void testIeee14() {
         Network network = IeeeCdfNetworkFactory.create14();
-        LoadFlowResult result = loadFlowRunner.run(network, parameters);
-        assertTrue(result.isOk());
+        LfNetwork lfNetwork = LfNetwork.load(network, new LfNetworkLoaderImpl(), new FirstSlackBusSelector()).get(0);
+        VoltageMagnitudeInitializer initializer = new VoltageMagnitudeInitializer(new DenseMatrixFactory());
+        initializer.prepare(lfNetwork);
+        assertBusVoltage(lfNetwork, initializer, "VL1_0", 1.06, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL2_0", 1.045, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL3_0", 1.01, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL4_0", 1.0351, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL5_0", 1.0356, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL6_0", 1.07, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL7_0", 1.074, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL8_0", 1.09, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL9_0", 1.0723, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL10_0", 1.0719, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL11_0", 1.0709, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL12_0", 1.0701, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL13_0", 1.0703, 0);
+        assertBusVoltage(lfNetwork, initializer, "VL14_0", 1.0714, 0);
     }
 }
