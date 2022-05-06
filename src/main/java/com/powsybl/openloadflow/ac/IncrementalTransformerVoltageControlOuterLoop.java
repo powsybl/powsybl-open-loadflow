0e5fca4687e6aca5fcb3bd1064a96b6db8934062
Anne Tilloy <anne.tilloy@rte-france.com>
2022-05-06 18:28:24 +0200

    Fix some code smells.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/IncrementalTransformerVoltageControlOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/ac/IncrementalTransformerVoltageControlOuterLoop.java
@@ -48,8 +48,7 @@ public class IncrementalTransformerVoltageControlOuterLoop extends AbstractTrans
 
     @Override
     public OuterLoopStatus check(OuterLoopContext context, Reporter reporter) {
-        OuterLoopStatus status =  changeTapPositions(context.getNetwork(), context.getAcLoadFlowContext().getEquationSystem(), context.getAcLoadFlowContext().getJacobianMatrix());
-        return status;
+        return changeTapPositions(context.getNetwork(), context.getAcLoadFlowContext().getEquationSystem(), context.getAcLoadFlowContext().getJacobianMatrix());
     }
 
     private OuterLoopStatus changeTapPositions(LfNetwork network, EquationSystem<AcVariableType, AcEquationType> equationSystem,
@@ -59,39 +58,37 @@ public class IncrementalTransformerVoltageControlOuterLoop extends AbstractTrans
         DenseMatrix sensitivities = getSensitivityValues(controllerBranches, equationSystem, j);
 
         for (LfBus bus : network.getBuses()) {
-            if (bus.isTransformerVoltageControlled()) {
-                if (bus.getTransformerVoltageControl().isPresent()) {
-                    TransformerVoltageControl voltageControl = bus.getTransformerVoltageControl().get();
-                    double targetV = voltageControl.getTargetValue();
-                    double voltage = voltageControl.getControlled().getV();
-                    double difference = targetV - voltage;
-                    List<LfBranch> controllers = voltageControl.getControllers();
-                    if (controllers.size() == 1) {
-                        double sensitivity = ((EquationTerm<AcVariableType, AcEquationType>) bus.getCalculatedV())
-                                .calculateSensi(sensitivities, controllers.indexOf(controllers.get(0)));
-                        PiModel piModel = controllers.get(0).getPiModel();
-                        double deltaR = difference / sensitivity;
-                        Pair<Boolean, Double> result = piModel.updateTapPositionR(deltaR, MAX_INCREMENT);
-                        if (result.getLeft()) {
-                            status = OuterLoopStatus.UNSTABLE;
-                        }
-                    } else {
-                        // several transformers control the same bus.
-                        boolean hasChanged = true;
-                        while (hasChanged) {
-                            for (LfBranch controller : controllers) {
-                                double sensitivity = ((EquationTerm<AcVariableType, AcEquationType>) voltageControl.getControlled().getCalculatedV())
-                                        .calculateSensi(sensitivities, controllerBranches.indexOf(controller));
-                                PiModel piModel = controller.getPiModel();
-                                double deltaR = difference / sensitivity;
-                                Pair<Boolean, Double> result = piModel.updateTapPositionR(deltaR, 1);
-                                difference = difference - result.getRight() * sensitivity;
-                                if (result.getLeft()) {
-                                    hasChanged = true;
-                                    status = OuterLoopStatus.UNSTABLE;
-                                } else {
-                                    hasChanged = false;
-                                }
+            if (bus.isTransformerVoltageControlled() && bus.getTransformerVoltageControl().isPresent()) {
+                TransformerVoltageControl voltageControl = bus.getTransformerVoltageControl().get();
+                double targetV = voltageControl.getTargetValue();
+                double voltage = voltageControl.getControlled().getV();
+                double difference = targetV - voltage;
+                List<LfBranch> controllers = voltageControl.getControllers();
+                if (controllers.size() == 1) {
+                    double sensitivity = ((EquationTerm<AcVariableType, AcEquationType>) bus.getCalculatedV())
+                            .calculateSensi(sensitivities, controllers.indexOf(controllers.get(0)));
+                    PiModel piModel = controllers.get(0).getPiModel();
+                    double deltaR = difference / sensitivity;
+                    Pair<Boolean, Double> result = piModel.updateTapPositionR(deltaR, MAX_INCREMENT);
+                    if (result.getLeft()) {
+                        status = OuterLoopStatus.UNSTABLE;
+                    }
+                } else {
+                    // several transformers control the same bus.
+                    boolean hasChanged = true;
+                    while (hasChanged) {
+                        for (LfBranch controller : controllers) {
+                            double sensitivity = ((EquationTerm<AcVariableType, AcEquationType>) voltageControl.getControlled().getCalculatedV())
+                                    .calculateSensi(sensitivities, controllerBranches.indexOf(controller));
+                            PiModel piModel = controller.getPiModel();
+                            double deltaR = difference / sensitivity;
+                            Pair<Boolean, Double> result = piModel.updateTapPositionR(deltaR, 1);
+                            difference = difference - result.getRight() * sensitivity;
+                            if (result.getLeft()) {
+                                hasChanged = true;
+                                status = OuterLoopStatus.UNSTABLE;
+                            } else {
+                                hasChanged = false;
                             }
                         }
                     }
@@ -108,7 +105,7 @@ public class IncrementalTransformerVoltageControlOuterLoop extends AbstractTrans
         for (LfBranch branch : controllerBranches) {
             Variable var = equationSystem.getVariable(branch.getNum(), AcVariableType.BRANCH_RHO1);
             for (Equation<AcVariableType, AcEquationType> equation : equationSystem.getSortedEquationsToSolve().keySet()) {
-                equation.getTerms().stream().filter(term -> term.getVariables().stream().filter(v -> v.equals(var)).findAny().isPresent()).forEach(t -> {
+                equation.getTerms().stream().filter(term -> term.getVariables().stream().anyMatch(v -> v.equals(var))).forEach(t -> {
                     if (equation.getType().equals(AcEquationType.BRANCH_TARGET_RHO1)) {
                         rhs.set(equation.getColumn(), controllerBranches.indexOf(branch), t.der(var));
                     }
