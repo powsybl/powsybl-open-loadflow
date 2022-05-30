9b6203bacfa971d232c7427a97b635f4a1082850
Anne Tilloy <48123713+annetill@users.noreply.github.com>
2022-05-30 16:37:55 +0200

    Remove transformer voltage controls when no PV buses at non controlled side (#482)
    * Remove transformer voltage controls.
    * Support of contingencies.
    * Add a unit test to show how AC sensis behave.
    * Fix AC sensitivity analysis for pre contingency computation.
    * SKIP instead of ZERO for sensitivity when controlled bus is no more controlled
    
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>
    Co-authored-by: Hadrien <hadrien.godard@artelys.com>
    Co-authored-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/SimpleTransformerVoltageControlOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/ac/SimpleTransformerVoltageControlOuterLoop.java
@@ -26,6 +26,7 @@ public class SimpleTransformerVoltageControlOuterLoop extends AbstractTransforme
         for (LfBranch controllerBranch : getControllerBranches(context.getNetwork())) {
             controllerBranch.setVoltageControlEnabled(true);
         }
+        context.getNetwork().fixTransformerVoltageControls();
     }
 
     @Override
