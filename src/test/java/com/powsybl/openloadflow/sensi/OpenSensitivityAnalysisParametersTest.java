4f5bdaea7f431cdd02249bfc14af70b67901a399
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-06-03 16:32:49 +0200

    Fix licence and author
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/sensi/OpenSensitivityAnalysisParametersTest.java
+++ src/test/java/com/powsybl/openloadflow/sensi/OpenSensitivityAnalysisParametersTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openloadflow.sensi;
 
 import com.google.common.jimfs.Configuration;
@@ -10,6 +16,9 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.FileSystem;
 
+/**
+ * @author Etienne Lesot <etienne.lesot at rte-france.com>
+ */
 public class OpenSensitivityAnalysisParametersTest {
     private InMemoryPlatformConfig platformConfig;
 
