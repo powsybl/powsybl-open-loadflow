0232c8f4f65743c9a0c723803070372da290647d
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-06-05 21:29:35 +0200

    Fix
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/ClosedBranchSide2CurrentMagnitudeEquationTerm.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/ClosedBranchSide2CurrentMagnitudeEquationTerm.java
@@ -135,15 +135,15 @@ public class ClosedBranchSide2CurrentMagnitudeEquationTerm extends AbstractClose
     public double der(Variable<AcVariableType> variable) {
         Objects.requireNonNull(variable);
         if (variable.equals(v1Var)) {
-            return di2dv1(y, ksi, g2, b2, v1(), a1(), r1(), a1(), v2(), ph2());
+            return di2dv1(y, ksi, g2, b2, v1(), ph1(), r1(), a1(), v2(), ph2());
         } else if (variable.equals(v2Var)) {
-            return di2dv2(y, ksi, g2, b2, v1(), a1(), r1(), a1(), v2(), ph2());
+            return di2dv2(y, ksi, g2, b2, v1(), ph1(), r1(), a1(), v2(), ph2());
         } else if (variable.equals(ph1Var)) {
-            return di2dph1(y, ksi, g2, b2, v1(), a1(), r1(), a1(), v2(), ph2());
+            return di2dph1(y, ksi, g2, b2, v1(), ph1(), r1(), a1(), v2(), ph2());
         } else if (variable.equals(ph2Var)) {
-            return di2dph2(y, ksi, g2, b2, v1(), a1(), r1(), a1(), v2(), ph2());
+            return di2dph2(y, ksi, g2, b2, v1(), ph1(), r1(), a1(), v2(), ph2());
         } else if (variable.equals(a1Var)) {
-            return di2da1(y, ksi, g2, b2, v1(), a1(), r1(), a1(), v2(), ph2());
+            return di2da1(y, ksi, g2, b2, v1(), ph1(), r1(), a1(), v2(), ph2());
         } else {
             throw new IllegalStateException("Unknown variable: " + variable);
         }
