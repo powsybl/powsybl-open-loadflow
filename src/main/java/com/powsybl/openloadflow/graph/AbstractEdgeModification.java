f44fd7e19904505cd4c724081c8af284ae490fb0
Florian Dupuy <florian.dupuy@rte-france.com>
2022-08-01 18:16:47 +0200

    Fix missing header
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/graph/AbstractEdgeModification.java
+++ src/main/java/com/powsybl/openloadflow/graph/AbstractEdgeModification.java
@@ -1,5 +1,14 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openloadflow.graph;
 
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
 public abstract class AbstractEdgeModification<V, E>  implements GraphModification<V, E>  {
     protected final E e;
     protected final V v1;
