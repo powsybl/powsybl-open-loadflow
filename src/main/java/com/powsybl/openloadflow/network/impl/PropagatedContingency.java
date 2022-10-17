f2735ab734ed2da8bc987b5a0028abe61f7826e7
Anne Tilloy <anne.tilloy@rte-france.com>
2022-10-17 16:25:08 +0200

    Fix code smell.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/network/impl/PropagatedContingency.java
+++ src/main/java/com/powsybl/openloadflow/network/impl/PropagatedContingency.java
@@ -294,7 +294,7 @@ public class PropagatedContingency {
 
         // we should manage branches open at one side.
         for (LfBus bus : buses) {
-            bus.getBranches().stream().filter(b -> !b.isConnectedAtBothSides()).forEach(b -> branches.add(b));
+            bus.getBranches().stream().filter(b -> !b.isConnectedAtBothSides()).forEach(branches::add);
         }
 
         // reset connectivity to discard triggered branches
