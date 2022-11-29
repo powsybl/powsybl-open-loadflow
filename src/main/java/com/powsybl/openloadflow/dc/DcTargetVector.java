8cc8ab48bee52d82c7381d8a104a546e35fd8657
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-11-29 13:23:47 +0100

    Fix license headers
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/dc/DcTargetVector.java
+++ src/main/java/com/powsybl/openloadflow/dc/DcTargetVector.java
@@ -2,6 +2,7 @@
  * Copyright (c) 2022, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * SPDX-License-Identifier: MPL-2.0
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.openloadflow.dc;
