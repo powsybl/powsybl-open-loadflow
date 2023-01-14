cc31ab2d35d193b04f708aa16a235e1610d1998d
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-01-14 13:24:05 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/test/java/com/powsybl/openloadflow/ac/SecondaryVoltageControlTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/SecondaryVoltageControlTest.java
@@ -8,6 +8,7 @@ package com.powsybl.openloadflow.ac;
 
 import com.powsybl.ieeecdf.converter.IeeeCdfNetworkFactory;
 import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Generator;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.extensions.SecondaryVoltageControl.ControlUnit;
 import com.powsybl.iidm.network.extensions.SecondaryVoltageControl.ControlZone;
@@ -35,6 +36,13 @@ class SecondaryVoltageControlTest {
 
     private Network network;
 
+    private Bus b4;
+    private Bus b6;
+    private Bus b8;
+    private Bus b10;
+    private Generator g6;
+    private Generator g8;
+
     private LoadFlow.Runner loadFlowRunner;
 
     private LoadFlowParameters parameters;
@@ -44,6 +52,13 @@ class SecondaryVoltageControlTest {
     @BeforeEach
     void setUp() {
         network = IeeeCdfNetworkFactory.create14();
+        b4 = network.getBusBreakerView().getBus("B4");
+        b6 = network.getBusBreakerView().getBus("B6");
+        b8 = network.getBusBreakerView().getBus("B8");
+        b10 = network.getBusBreakerView().getBus("B10");
+        g6 = network.getGenerator("B6-G");
+        g8 = network.getGenerator("B8-G");
+
         // adjust reactive limit to avoid generators be to limit
         var g1 = network.getGenerator("B1-G");
         g1.newMinMaxReactiveLimits()
@@ -53,37 +68,30 @@ class SecondaryVoltageControlTest {
         var g2 = network.getGenerator("B2-G");
         g2.newMinMaxReactiveLimits()
                 .setMinQ(-40)
-                .setMaxQ(30)
+                .setMaxQ(35)
                 .add();
-        network.getGenerator("B3-G");
-        var g6 = network.getGenerator("B6-G");
         g6.newMinMaxReactiveLimits()
-                .setMinQ(-60)
+                .setMinQ(-58)
                 .setMaxQ(24)
                 .add();
-        var g8 = network.getGenerator("B8-G");
         g8.newMinMaxReactiveLimits()
                 .setMinQ(-6)
                 .setMaxQ(200)
                 .add();
+
         loadFlowRunner = new LoadFlow.Runner(new OpenLoadFlowProvider(new DenseMatrixFactory()));
-        parameters = new LoadFlowParameters()
-                .setUseReactiveLimits(false);
+        parameters = new LoadFlowParameters();
         parametersExt = OpenLoadFlowParameters.create(parameters);
     }
 
     @Test
-    void test() {
+    void testNoReactiveLimits() {
+        parameters.setUseReactiveLimits(false);
         PilotPoint pilotPoint = new PilotPoint(List.of("B10"), 14.4);
         network.newExtension(SecondaryVoltageControlAdder.class)
                 .addControlZone(new ControlZone("z1", pilotPoint, List.of(new ControlUnit("B6-G"),
                                                                           new ControlUnit("B8-G"))))
                 .add();
-        Bus b6 = network.getBusBreakerView().getBus("B6");
-        Bus b8 = network.getBusBreakerView().getBus("B8");
-        Bus b10 = network.getBusBreakerView().getBus("B10");
-        var g6 = network.getGenerator("B6-G");
-        var g8 = network.getGenerator("B8-G");
 
         LoadFlowResult result = loadFlowRunner.run(network, parameters);
         assertEquals(LoadFlowResult.ComponentResult.Status.CONVERGED, result.getComponentResults().get(0).getStatus());
@@ -119,16 +127,14 @@ class SecondaryVoltageControlTest {
     }
 
     @Test
-    void multiZonesTest() {
+    void multiNoReactiveLimitsZonesTest() {
+        parameters.setUseReactiveLimits(false);
         PilotPoint pilotPoint1 = new PilotPoint(List.of("B4"), 142);
         PilotPoint pilotPoint2 = new PilotPoint(List.of("B10"), 14.5);
         network.newExtension(SecondaryVoltageControlAdder.class)
                 .addControlZone(new ControlZone("z1", pilotPoint1, List.of(new ControlUnit("B1-G"), new ControlUnit("B2-G"), new ControlUnit("B3-G"))))
                 .addControlZone(new ControlZone("z2", pilotPoint2, List.of(new ControlUnit("B6-G"), new ControlUnit("B8-G"))))
                 .add();
-
-        Bus b4 = network.getBusBreakerView().getBus("B4");
-        Bus b10 = network.getBusBreakerView().getBus("B10");
 
         parametersExt.setSecondaryVoltageControl(true);
         var result = loadFlowRunner.run(network, parameters);
