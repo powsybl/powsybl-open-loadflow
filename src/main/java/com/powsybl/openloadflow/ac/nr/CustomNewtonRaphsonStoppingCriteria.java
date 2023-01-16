7599960148811ba82acb817c7c20b55fb5e4057e
Alexandre Le Jean <alexandre.le-jean@artelys.com>
2023-01-16 08:34:32 +0100

    fix(OPLF): added Licence Identifier and fix author in file CustomNewtonRaphsonStoppingCriteria.java
    Signed-off-by: Alexandre Le Jean <alexandre.le-jean@artelys.com>

--- src/main/java/com/powsybl/openloadflow/ac/nr/CustomNewtonRaphsonStoppingCriteria.java
+++ src/main/java/com/powsybl/openloadflow/ac/nr/CustomNewtonRaphsonStoppingCriteria.java
@@ -3,6 +3,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 package com.powsybl.openloadflow.ac.nr;
 
@@ -12,7 +13,7 @@ import com.powsybl.openloadflow.equations.EquationSystem;
 import com.powsybl.openloadflow.equations.Vectors;
 
 /**
- * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ * @author Alexandre Le Jean <alexandre.le-jean at artelys.com>
  */
 public class CustomNewtonRaphsonStoppingCriteria implements NewtonRaphsonStoppingCriteria {
 
