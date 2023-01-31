96d63ae46262ddd85506ea240d51f1ec1c127223
Bertrand Rix <bertrand.rix@artelys.com>
2023-01-31 11:53:31 +0100

    Update number of parameters after rebase.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- src/test/java/com/powsybl/openloadflow/OpenLoadFlowProviderTest.java
+++ src/test/java/com/powsybl/openloadflow/OpenLoadFlowProviderTest.java
@@ -85,7 +85,7 @@ class OpenLoadFlowProviderTest {
     @Test
     void specificParametersTest() {
         OpenLoadFlowProvider provider = new OpenLoadFlowProvider();
-        assertEquals(32, provider.getSpecificParameters().size());
+        assertEquals(33, provider.getSpecificParameters().size());
         LoadFlowParameters parameters = new LoadFlowParameters();
 
         provider.loadSpecificParameters(Collections.emptyMap())
