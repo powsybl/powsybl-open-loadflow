ceb4312b8134957941590b2b0ba4847223422481
Anne Tilloy <48123713+annetill@users.noreply.github.com>
2023-02-09 21:13:37 +0100

    Change default value for max Pq Pv switch and add a parameter (#728)
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/OpenLoadFlowProviderTest.java
+++ src/test/java/com/powsybl/openloadflow/OpenLoadFlowProviderTest.java
@@ -85,7 +85,7 @@ class OpenLoadFlowProviderTest {
     @Test
     void specificParametersTest() {
         OpenLoadFlowProvider provider = new OpenLoadFlowProvider();
-        assertEquals(29, provider.getSpecificParameters().size());
+        assertEquals(30, provider.getSpecificParameters().size());
         LoadFlowParameters parameters = new LoadFlowParameters();
 
         provider.loadSpecificParameters(Collections.emptyMap())
