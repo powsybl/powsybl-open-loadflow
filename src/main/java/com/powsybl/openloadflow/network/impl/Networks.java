99f4a20029f47657649a26b880ca89bbedb8bfd9
Anne Tilloy <anne.tilloy@rte-france.com>
2023-02-28 14:51:49 +0100

    Bump to core 5.2.0-SNAPSHOT
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/network/impl/Networks.java
+++ src/main/java/com/powsybl/openloadflow/network/impl/Networks.java
@@ -12,6 +12,7 @@ import com.powsybl.iidm.network.*;
 import com.powsybl.openloadflow.network.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -60,7 +61,7 @@ public final class Networks {
         resetInjectionsState(network.getLoads());
         resetInjectionsState(network.getLccConverterStations());
         resetInjectionsState(network.getBatteries());
-        resetInjectionsState(network.getDanglingLines());
+        resetInjectionsState(network.getDanglingLineStream().filter(dl -> !dl.isMerged()).collect(Collectors.toSet()));
     }
 
     private static double getDoubleProperty(Identifiable<?> identifiable, String name) {
