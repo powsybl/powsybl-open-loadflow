7a67b131fd8e8ff96aadfb8886e3de8cf290cc56
Caio Luke <caio.luke@artelys.com>
2023-03-07 12:33:03 +0100

    refactor metrix security analysis network factory
    Signed-off-by: Caio Luke <caio.luke@artelys.com>

--- src/test/java/com/powsybl/openloadflow/network/MetrixTutorialSixBusesSecurityAnalysisFactory.java
+++ src/test/java/com/powsybl/openloadflow/network/MetrixTutorialSixBusesSecurityAnalysisFactory.java
@@ -1,6 +1,7 @@
 package com.powsybl.openloadflow.network;
 
 import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.Line;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.TwoWindingsTransformer;
 import com.powsybl.iidm.xml.test.MetrixTutorialSixBusesFactory;
@@ -15,84 +16,77 @@ public class MetrixTutorialSixBusesSecurityAnalysisFactory extends AbstractLoadF
     }
 
     public static Network create(MetrixViolations metrixViolation) {
+        Network network = MetrixTutorialSixBusesFactory.create();
+
+        String[] idSpecialLines;
+        double specialR;
+        double normalLineLimit;
+        double specialLineLimit;
+
         switch (metrixViolation) {
-            case CURRENT:
+            case CURRENT: {
+                idSpecialLines = new String[]{"S_SO_1", "S_SO_2"};
+                specialR = 0.2;
+                normalLineLimit = 400.0;
+                specialLineLimit = 350.0;
+                network.getGenerator("SO_G2").setTargetP(680.0);
+                break; }
             case ACTIVE_POWER:
-            case APPARENT_POWER:
-                return createWithCurrentActivePowerApparentPowerViolation(metrixViolation);
-            case VOLTAGE:
-                return createWithVoltageViolation();
+            case APPARENT_POWER: {
+                idSpecialLines = new String[]{"S_SO_1", "S_SO_2"};
+                specialR = 0.2;
+                normalLineLimit = 300.0;
+                specialLineLimit = 250.0;
+                network.getGenerator("SO_G2").setTargetP(680.0);
+                break; }
+            case VOLTAGE: {
+                idSpecialLines = new String[]{"S_SE_1", "S_SE_2", "SE_NE_1", "SE_NE_2"};
+                specialR = 90;
+                normalLineLimit = 1000.0;
+                specialLineLimit = 2000.0;
+                network.getGenerator("SO_G2").setTargetP(120.0);
+                network.getGenerator("N_G").setVoltageRegulatorOn(false).setTargetP(600.0);
+                network.getGenerator("SE_G").setVoltageRegulatorOn(false).setTargetP(50.0);
+                network.getLoad("SE_L1").setP0(1500);
+                break; }
             default:
                 throw new PowsyblException("metrixViolation not defined");
         }
-    }
 
-    public static Network createWithCurrentActivePowerApparentPowerViolation(MetrixViolations metrixViolation) {
-        Network network = MetrixTutorialSixBusesFactory.create();
-        network.getGenerator("SO_G2").setTargetP(680.0);
-        String[] idLines = {"NO_N_1", "NO_N_2", "S_SE_1", "S_SE_2", "S_SO_1", "S_SO_2", "SO_NO_1", "SO_NO_2", "NE_N_1", "NE_N_2", "SE_NE_1", "SE_NE_2"};
-        for (String idLine : idLines) {
-            network.getLine(idLine).setR(0.2);
-            switch (metrixViolation) {
-                case CURRENT:
-                    network.getLine(idLine).newCurrentLimits1().setPermanentLimit(400.0).add();
-                    network.getLine(idLine).newCurrentLimits2().setPermanentLimit(400.0).add();
-                    break;
-                case ACTIVE_POWER:
-                    network.getLine(idLine).newActivePowerLimits1().setPermanentLimit(300.0).add();
-                    network.getLine(idLine).newActivePowerLimits2().setPermanentLimit(300.0).add();
-                    break;
-                case APPARENT_POWER:
-                    network.getLine(idLine).newApparentPowerLimits1().setPermanentLimit(300.0).add();
-                    network.getLine(idLine).newApparentPowerLimits2().setPermanentLimit(300.0).add();
-                    break;
-            }
+        // change resistance and add limits to all lines
+        for (Line line : network.getLines()) {
+            line.setR(0.2);
+            addLineLimits(line, normalLineLimit, metrixViolation);
         }
-        String[] idSpecialLines = {"S_SO_1", "S_SO_2"};
-        for (String idLine : idSpecialLines) {
-            network.getLine(idLine).setR(0.1);
-            switch (metrixViolation) {
-                case CURRENT:
-                    network.getLine(idLine).newCurrentLimits1().setPermanentLimit(350.0).add();
-                    network.getLine(idLine).newCurrentLimits2().setPermanentLimit(350.0).add();
-                    break;
-                case ACTIVE_POWER:
-                    network.getLine(idLine).newActivePowerLimits1().setPermanentLimit(250.0).add();
-                    network.getLine(idLine).newActivePowerLimits2().setPermanentLimit(250.0).add();
-                    break;
-                case APPARENT_POWER:
-                    network.getLine(idLine).newApparentPowerLimits1().setPermanentLimit(250.0).add();
-                    network.getLine(idLine).newApparentPowerLimits2().setPermanentLimit(250.0).add();
-                    break;
-            }
-        }
-        TwoWindingsTransformer transfo = network.getTwoWindingsTransformer("NE_NO_1");
-        transfo.setR(0.1);
-        return network;
-    }
 
-    public static Network createWithVoltageViolation() {
-        Network network = MetrixTutorialSixBusesFactory.create();
-        network.getGenerator("SO_G2").setTargetP(120.0);
-        network.getGenerator("N_G").setVoltageRegulatorOn(false).setTargetP(600.0);
-        network.getGenerator("SE_G").setVoltageRegulatorOn(false).setTargetP(50.0);
-        network.getLoad("SE_L1").setP0(1500);
-        String[] idLines = {"NO_N_1", "NO_N_2", "S_SE_1", "S_SE_2", "S_SO_1", "S_SO_2", "SO_NO_1", "SO_NO_2", "NE_N_1", "NE_N_2", "SE_NE_1", "SE_NE_2"};
+        // change resistance and add limits to some lines
+        for (String idSpecialLine : idSpecialLines) {
+            Line line = network.getLine(idSpecialLine);
+            line.setR(specialR);
+            addLineLimits(line, specialLineLimit, metrixViolation);
+        }
 
-        for (String idLine : idLines) {
-            network.getLine(idLine).setR(0.2);
-            network.getLine(idLine).newCurrentLimits1().setPermanentLimit(1000.0).add();
-            network.getLine(idLine).newCurrentLimits2().setPermanentLimit(1000.0).add();
-        }
-        String[] idSpecialLines = {"S_SE_1", "S_SE_2", "SE_NE_1", "SE_NE_2"};
-        for (String idLine : idSpecialLines) {
-            network.getLine(idLine).setR(90);
-            network.getLine(idLine).newCurrentLimits1().setPermanentLimit(2000.0).add();
-            network.getLine(idLine).newCurrentLimits2().setPermanentLimit(2000.0).add();
-        }
         TwoWindingsTransformer transfo = network.getTwoWindingsTransformer("NE_NO_1");
         transfo.setR(0.1);
 
         return network;
+    }
+
+    private static void addLineLimits(Line line, double limit, MetrixViolations metrixViolation) {
+        switch (metrixViolation) {
+            case CURRENT:
+            case VOLTAGE:
+                line.newCurrentLimits1().setPermanentLimit(limit).add();
+                line.newCurrentLimits2().setPermanentLimit(limit).add();
+                break;
+            case ACTIVE_POWER:
+                line.newActivePowerLimits1().setPermanentLimit(limit).add();
+                line.newActivePowerLimits2().setPermanentLimit(limit).add();
+                break;
+            case APPARENT_POWER:
+                line.newApparentPowerLimits1().setPermanentLimit(limit).add();
+                line.newApparentPowerLimits2().setPermanentLimit(limit).add();
+                break;
+        }
     }
 }
