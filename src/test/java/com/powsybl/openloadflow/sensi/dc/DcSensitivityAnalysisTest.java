1715c8f9eda273d825c6408fbe5124ec82bc1d7f
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-04-12 11:38:53 +0200

    Fix no impact load contingency when simulated with a switch contingency (#768)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/sensi/dc/DcSensitivityAnalysisTest.java
+++ src/test/java/com/powsybl/openloadflow/sensi/dc/DcSensitivityAnalysisTest.java
@@ -828,8 +828,7 @@ class DcSensitivityAnalysisTest extends AbstractSensitivityAnalysisTest {
         Network network = NodeBreakerNetworkFactory.create();
         List<Contingency> contingencies = List.of(new Contingency("c1", new BranchContingency("L1")));
 
-        List<PropagatedContingency> propagatedContingencies = PropagatedContingency.createList(network, contingencies,
-                Collections.emptySet(), Collections.emptySet(), false, false, false, false);
+        List<PropagatedContingency> propagatedContingencies = PropagatedContingency.createList(network, contingencies, Collections.emptySet(), false);
         assertEquals(1, propagatedContingencies.size());
     }
 
