f5e93975653848816ada275e494a44301f8dfe93
JB-H <jbheyberger@gmail.com>
2023-05-05 23:04:38 +0200

    copyright 2
    Signed-off-by: JB-H <jbheyberger@gmail.com>

--- src/main/java/com/powsybl/openloadflow/util/ComplexPart.java
+++ src/main/java/com/powsybl/openloadflow/util/ComplexPart.java
@@ -8,7 +8,7 @@
 package com.powsybl.openloadflow.util;
 
 /**
- * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at gmail.com>
  */
 public enum ComplexPart {
     REAL,
