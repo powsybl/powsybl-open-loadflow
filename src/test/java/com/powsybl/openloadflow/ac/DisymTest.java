f95a0937fb2050d9cd1b4303535ff2f5dff1d58b
JB-H <jbheyberger@gmail.com>
2023-05-10 18:38:55 +0200

    ComplexMatrix Test
    Signed-off-by: JB-H <jbheyberger@gmail.com>

--- src/test/java/com/powsybl/openloadflow/ac/DisymTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/DisymTest.java
@@ -1089,38 +1089,6 @@ public class DisymTest {
     }
 
     @Test
-    void complexMatrixTest() {
-        DenseMatrix fortescueMatrix = Fortescue.createMatrix();
-        ComplexMatrix complexFortescueMatrix = Fortescue.createComplexMatrix(false);
-        System.out.println("Matrix Fortescue = ");
-        fortescueMatrix.print(System.out);
-
-        System.out.println("Matrix Complex Fortescue = ");
-        DenseMatrix computedMatrix = complexFortescueMatrix.getRealCartesianMatrix();
-
-        for (int i = 0; i < fortescueMatrix.getRowCount(); i++) {
-            for (int j = 0; j < fortescueMatrix.getColumnCount(); j++) {
-                assertEquals(computedMatrix.get(i, j), fortescueMatrix.get(i, j));
-            }
-        }
-    }
-
-    @Test
-    void realToComplexMatrixTest() {
-        ComplexMatrix complexFortescueMatrix = Fortescue.createComplexMatrix(false);
-        DenseMatrix realFortescueMatrix = complexFortescueMatrix.getRealCartesianMatrix();
-
-        ComplexMatrix computedComplexFortescueMatrix = ComplexMatrix.getComplexMatrixFromRealCartesian(realFortescueMatrix);
-        DenseMatrix computedMatrix = computedComplexFortescueMatrix.getRealCartesianMatrix();
-
-        for (int i = 0; i < realFortescueMatrix.getRowCount(); i++) {
-            for (int j = 0; j < realFortescueMatrix.getColumnCount(); j++) {
-                assertEquals(computedMatrix.get(i, j), realFortescueMatrix.get(i, j));
-            }
-        }
-    }
-
-    @Test
     void asym3phaseTransfoTest() {
         // test of an asymmetric three phase transformer
         Complex za = new Complex(0.1, 1.);
