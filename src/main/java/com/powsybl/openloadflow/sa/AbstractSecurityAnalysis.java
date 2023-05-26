fc1ec04cefe1f5c1cfec4e04511130d08ba3ef62
Anne Tilloy <anne.tilloy@rte-france.com>
2023-05-26 13:59:07 +0200

    Fix code smell.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/sa/AbstractSecurityAnalysis.java
+++ src/main/java/com/powsybl/openloadflow/sa/AbstractSecurityAnalysis.java
@@ -132,10 +132,8 @@ public abstract class AbstractSecurityAnalysis<V extends Enum<V> & Quantity, E e
 
                 case LineConnectionAction.NAME: {
                     LineConnectionAction lineConnectionAction = (LineConnectionAction) action;
-                    if (network.getBranch(lineConnectionAction.getLineId()) == null) {
-                        if (network.getTieLine(lineConnectionAction.getLineId()) == null) {
-                            throw new PowsyblException("Branch '" + lineConnectionAction.getLineId() + "' not found");
-                        }
+                    if (network.getBranch(lineConnectionAction.getLineId()) == null && network.getTieLine(lineConnectionAction.getLineId()) == null) {
+                        throw new PowsyblException("Branch '" + lineConnectionAction.getLineId() + "' not found");
                     }
                     break;
                 }
