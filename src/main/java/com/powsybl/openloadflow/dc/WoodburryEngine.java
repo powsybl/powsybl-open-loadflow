cfbd6ca49f8e336bcf87a6701b399243bcab225f
Hadrien <hadrien.godard@artelys.com>
2023-06-05 17:40:31 +0200

    Start
    Signed-off-by: Hadrien <hadrien.godard@artelys.com>

---
+++ src/main/java/com/powsybl/openloadflow/dc/WoodburryEngine.java
@@ -0,0 +1,10 @@
+package com.powsybl.openloadflow.dc;
+
+import com.powsybl.math.matrix.LUDecomposition;
+
+public class WoodburryEngine {
+
+    public void compute(LUDecomposition lu) {
+
+    }
+}
