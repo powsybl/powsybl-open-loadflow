ef85ba43d97daa5043b06d9520a03f875a1c87af
Anne Tilloy <anne.tilloy@rte-france.com>
2023-06-12 14:11:59 +0200

    Fix proposal.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/network/util/ZeroImpedanceFlows.java
+++ src/main/java/com/powsybl/openloadflow/network/util/ZeroImpedanceFlows.java
@@ -131,8 +131,11 @@ public class ZeroImpedanceFlows {
         private PQ balanceWithImpedance(LfBus bus, LoadFlowModel loadFlowModel) {
             // balance considering injections and flow from lines with impedance
 
+            double qShunt = bus.getShunt().map(shunt -> shunt.getQ().eval()).orElse(0.0);
+            qShunt += bus.getControllerShunt().map(shunt -> shunt.getQ().eval()).orElse(0.0);
+            qShunt += bus.getSvcShunt().map(shunt -> shunt.getQ().eval()).orElse(0.0);
             // take care of the sign
-            PQ balancePQ = new PQ(-bus.getP().eval(), -bus.getQ().eval());
+            PQ balancePQ = new PQ(-bus.getP().eval(), -bus.getQ().eval() + qShunt);
 
             // only lines with impedance
             List<LfBranch> adjacentBranchesWithImpedance = bus.getBranches().stream()
