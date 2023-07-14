68ba3dfd69552d9db5255bdb7a96bafa455be6fd
JB-H <jbheyberger@gmail.com>
2023-07-14 22:24:00 +0200

    code coverage for Fortescue Transformers
    Signed-off-by: JB-H <jbheyberger@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/ClosedBranchTfoNegativeIflowEquationTerm.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/ClosedBranchTfoNegativeIflowEquationTerm.java
@@ -7,7 +7,6 @@ import com.powsybl.openloadflow.network.LfBranch;
 import com.powsybl.openloadflow.network.LfBus;
 import com.powsybl.openloadflow.util.ComplexMatrix;
 import com.powsybl.openloadflow.util.Fortescue;
-import net.jafama.FastMath;
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.complex.ComplexUtils;
 
@@ -49,46 +48,6 @@ public class ClosedBranchTfoNegativeIflowEquationTerm extends AbstractClosedBran
         DenseMatrix mTmp2 = mY.times(mTmp1);
 
         return mRhoConjugate.times(mTmp2); // expression of I
-    }
-
-    public static DenseMatrix getdIda1dr1(Complex y1, Complex y2, Complex y12, DenseMatrix mV, Complex r1, Complex r2, boolean isDeriveR1) {
-
-        // Supposing A2 = 0 and R2 is constant We have:
-        // [dI1x]   [ R1.cos(A1+Pi/2)  R1.sin(A1+Pi/2)     0       0    ] [ g1+g12  -b1-b12   -g12     b12   ] [ R1.cos(A1) -R1.sin(A1)     0       0    ]  [V1x]
-        // [dI1y]   [-R1.sin(A1+Pi/2)  R1.cos(A1+Pi/2)     0       0    ] [ b1+b12   g1+g12   -b12    -g12   ] [ R1.sin(A1)  R1.cos(A1)     0       0    ]  [V1y]
-        // [dI2x] = [        0                0            R2      0    ] [  -g21     b21    g2+g21  -b2-b21 ] [    0           0           R2      0    ]* [V2x]
-        // [dI2y]   [        0                0            0       R2   ] [  -b21    -g21    b2+b21   g2+g21 ] [    0           0           0       R2   ]  [V2y]
-
-        //         [ R1.cos(A1)  R1.sin(A1)  0       0    ] [ g1+g12  -b1-b12   -g12     b12   ] [ R1.cos(A1+Pi/2) -R1.sin(A1+Pi/2)     0       0    ]  [V1x]
-        //         [-R1.sin(A1)  R1.cos(A1)  0       0    ] [ b1+b12   g1+g12   -b12    -g12   ] [ R1.sin(A1+Pi/2)  R1.cos(A1+Pi/2)     0       0    ]  [V1y]
-        //       + [      0         0        R2      0    ] [  -g21     b21    g2+g21  -b2-b21 ] [        0               0             R2      0    ]* [V2x]
-        //         [      0         0        0       R2   ] [  -b21    -g21    b2+b21   g2+g21 ] [        0               0             0       R2   ]  [V2y]
-
-        DenseMatrix mRho = getRhoMatrix(r1, r2);
-        DenseMatrix mRhoConjugate = getRhoMatrix(r1.conjugate(), r2.conjugate());
-        Complex dr1 = ComplexUtils.polar2Complex(r1.abs(), r1.getArgument() + FastMath.PI / 2);
-        if (isDeriveR1) {
-            dr1 = ComplexUtils.polar2Complex(1., r1.getArgument());
-        }
-        DenseMatrix mdRho = getRhoMatrix(dr1, r2);
-        DenseMatrix mdRhoConjugate = getRhoMatrix(dr1.conjugate(), r2);
-        DenseMatrix mY = getFixedYmatrix(y1, y2, y12);
-
-        DenseMatrix mTmp1 = mRho.times(mV);
-        DenseMatrix mTmp2 = mY.times(mTmp1);
-        DenseMatrix mTmp3 = mdRhoConjugate.times(mTmp2);
-
-        DenseMatrix mTmp4 = mdRho.times(mV);
-        DenseMatrix mTmp5 = mY.times(mTmp4);
-        DenseMatrix mTmp6 = mRhoConjugate.times(mTmp5);
-
-        DenseMatrix mdI = new DenseMatrix(4, 0);
-        mdI.add(0, 0, mTmp3.get(0, 0) + mTmp6.get(0, 0));
-        mdI.add(1, 0, mTmp3.get(1, 0) + mTmp6.get(1, 0));
-        mdI.add(2, 0, mTmp3.get(2, 0) + mTmp6.get(2, 0));
-        mdI.add(3, 0, mTmp3.get(3, 0) + mTmp6.get(3, 0));
-
-        return mdI;
     }
 
     public static DenseMatrix getRhoMatrix(Complex r1, Complex r2) {
@@ -145,14 +104,19 @@ public class ClosedBranchTfoNegativeIflowEquationTerm extends AbstractClosedBran
     public double der(Variable<AcVariableType> variable) {
         Objects.requireNonNull(variable);
         if (variable.equals(r1Var) || variable.equals(a1Var)) {
-            boolean isDerivR1 = false;
-            if (variable.equals(r1Var)) {
-                isDerivR1 = true;
-            }
-            return getdIda1dr1(new Complex(g1, b1), new Complex(g2, b2), new Complex(g12, b12),
-                    getCartesianVoltageVector(v1(), ph1(), v2(), ph2()),
-                    ComplexUtils.polar2Complex(r1(), a1()),
-                    ComplexUtils.polar2Complex(R2, 0.), isDerivR1).get(getIndexline(flowType), 0);
+            throw new IllegalStateException("asymmetric Tfo with derivation of rho or phase shift not yet handled " + element.getId());
+            // formula to be implemented once rho and phase shift handled in asymmetric load flow
+            // Supposing A2 = 0 and R2 is constant We have:
+            // [dI1x]   [ R1.cos(A1+Pi/2)  R1.sin(A1+Pi/2)     0       0    ] [ g1+g12  -b1-b12   -g12     b12   ] [ R1.cos(A1) -R1.sin(A1)     0       0    ]  [V1x]
+            // [dI1y]   [-R1.sin(A1+Pi/2)  R1.cos(A1+Pi/2)     0       0    ] [ b1+b12   g1+g12   -b12    -g12   ] [ R1.sin(A1)  R1.cos(A1)     0       0    ]  [V1y]
+            // [dI2x] = [        0                0            R2      0    ] [  -g21     b21    g2+g21  -b2-b21 ] [    0           0           R2      0    ]* [V2x]
+            // [dI2y]   [        0                0            0       R2   ] [  -b21    -g21    b2+b21   g2+g21 ] [    0           0           0       R2   ]  [V2y]
+
+            //         [ R1.cos(A1)  R1.sin(A1)  0       0    ] [ g1+g12  -b1-b12   -g12     b12   ] [ R1.cos(A1+Pi/2) -R1.sin(A1+Pi/2)     0       0    ]  [V1x]
+            //         [-R1.sin(A1)  R1.cos(A1)  0       0    ] [ b1+b12   g1+g12   -b12    -g12   ] [ R1.sin(A1+Pi/2)  R1.cos(A1+Pi/2)     0       0    ]  [V1y]
+            //       + [      0         0        R2      0    ] [  -g21     b21    g2+g21  -b2-b21 ] [        0               0             R2      0    ]* [V2x]
+            //         [      0         0        0       R2   ] [  -b21    -g21    b2+b21   g2+g21 ] [        0               0             0       R2   ]  [V2y]
+
         } else {
             DenseMatrix mdV = getdVdx(variable);
             return getdIdv(new Complex(g1, b1), new Complex(g2, b2), mdV, new Complex(g12, b12),
