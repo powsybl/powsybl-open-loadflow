047569310bc3e9e257ca276a36caee6c1157f536
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-09-19 22:53:58 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/test/java/com/powsybl/openloadflow/ac/LoadModelTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/LoadModelTest.java
@@ -6,9 +6,9 @@
  */
 package com.powsybl.openloadflow.ac;
 
-import com.powsybl.cgmes.conformity.CgmesConformity1Catalog;
-import com.powsybl.commons.datasource.ReadOnlyDataSource;
-import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.network.Load;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import com.powsybl.loadflow.LoadFlow;
 import com.powsybl.loadflow.LoadFlowParameters;
@@ -20,11 +20,8 @@ import com.powsybl.openloadflow.network.EurostagFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static com.powsybl.openloadflow.util.LoadFlowAssert.assertActivePowerEquals;
-import static com.powsybl.openloadflow.util.LoadFlowAssert.assertReactivePowerEquals;
+import static com.powsybl.openloadflow.util.LoadFlowAssert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 /**
@@ -87,8 +84,10 @@ class LoadModelTest {
                 .add();
         LoadFlowResult result = loadFlowRunner.run(network, parameters);
         assertSame(LoadFlowResult.ComponentResult.Status.CONVERGED, result.getComponentResults().get(0).getStatus());
-        assertActivePowerEquals(47.768, expLoad.getTerminal());
-        assertReactivePowerEquals(28.661, expLoad.getTerminal());
+        assertVoltageEquals(141.68, expLoad.getTerminal().getBusView().getBus());
+        assertEquals(150, expLoad.getTerminal().getVoltageLevel().getNominalV(), 0);
+        assertActivePowerEquals(47.768, expLoad.getTerminal()); // < 50MW because v < vnom
+        assertReactivePowerEquals(28.661, expLoad.getTerminal()); // < 30MW because v < vnom
         assertActivePowerEquals(600, load.getTerminal());
         assertReactivePowerEquals(200, load.getTerminal());
     }
@@ -152,23 +151,5 @@ class LoadModelTest {
         assertSame(LoadFlowResult.ComponentResult.Status.CONVERGED, result.getComponentResults().get(0).getStatus());
         assertActivePowerEquals(600, zipLoad.getTerminal());
         assertReactivePowerEquals(200, zipLoad.getTerminal());
-    }
-
-    @Test
-    void microGridNlTest() {
-        ReadOnlyDataSource dataSource = CgmesConformity1Catalog.microGridBaseCaseNL()
-                .dataSource();
-        Network network = Network.read(dataSource);
-        Map<String, Double> vMap = network.getBusView().getBusStream()
-                .collect(Collectors.toMap(Identifiable::getId, Bus::getV));
-        LoadFlowResult result = loadFlowRunner.run(network, parameters);
-        assertSame(LoadFlowResult.ComponentResult.Status.CONVERGED, result.getComponentResults().get(0).getStatus());
-        Load l = network.getLoad("b1e03a8f-6a11-4454-af58-4a4a680e857f");
-        assertActivePowerEquals(489.123, l.getTerminal());
-        assertReactivePowerEquals(208.478, l.getTerminal());
-        for (Bus bus : network.getBusView().getBuses()) {
-            double dv = bus.getV() - vMap.get(bus.getId());
-//            assertEquals(0, dv, 1e-1);
-        }
     }
 }
