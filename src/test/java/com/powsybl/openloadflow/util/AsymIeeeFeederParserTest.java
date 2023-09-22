3ee96c2c517d39482008d8218e568def6a258b87
JB-H <jbheyberger@gmail.com>
2023-09-22 22:24:29 +0200

    use of sparse matrix for large parsed examples
    Signed-off-by: JB-H <jbheyberger@gmail.com>

--- src/test/java/com/powsybl/openloadflow/util/AsymIeeeFeederParserTest.java
+++ src/test/java/com/powsybl/openloadflow/util/AsymIeeeFeederParserTest.java
@@ -11,7 +11,6 @@ import com.powsybl.openloadflow.network.SlackBusSelectionMode;
 import com.powsybl.openloadflow.network.util.AsymIeeeFeederParser;
 import com.powsybl.openloadflow.network.util.AsymLvFeederParser;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.powsybl.openloadflow.util.LoadFlowAssert.assertVoltageEquals;
@@ -73,7 +72,6 @@ public class AsymIeeeFeederParserTest {
         assertVoltageEquals(4.298876625269438, network.getBusBreakerView().getBus("Bus-72"));
     }
 
-    @Disabled
     @Test
     void testLvFeedersTest() {
 
