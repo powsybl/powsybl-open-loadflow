08e7c22e25d6defa159de20286366c63edc8838e
Bertrand Rix <bertrand.rix@artelys.com>
2023-10-10 13:47:15 +0200

    Fix.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- src/main/java/com/powsybl/openloadflow/network/impl/OlfThreeWindingsTransformerResult.java
+++ src/main/java/com/powsybl/openloadflow/network/impl/OlfThreeWindingsTransformerResult.java
@@ -1,8 +1,17 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openloadflow.network.impl;
 
 import com.powsybl.commons.extensions.AbstractExtension;
 import com.powsybl.security.results.ThreeWindingsTransformerResult;
 
+/**
+ * @author Bertrand Rix <bertrand.rix at artelys.com>
+ */
 public class OlfThreeWindingsTransformerResult extends AbstractExtension<ThreeWindingsTransformerResult> {
 
     private final double v1;
