ab112e5272971fc328f5b259c5916e921b905e4f
Anne Tilloy <anne.tilloy@rte-france.com>
2023-10-11 11:04:37 +0200

    Clean and fix unit test (2).
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
+++ src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
@@ -2246,5 +2246,10 @@ class OpenSecurityAnalysisTest extends AbstractOpenSecurityAnalysisTest {
         LimitViolation limit2 = result2.getPostContingencyResults().get(0).getLimitViolationsResult().getLimitViolations().get(0);
         assertEquals(LimitViolationType.HIGH_VOLTAGE_ANGLE, limit2.getLimitType());
         assertEquals(7.498849, limit2.getValue(), 10E-6);
+
+        network.getVoltageAngleLimit("val").remove();
+        network.newVoltageAngleLimit().setId("val").from(line.getTerminal2()).to(line.getTerminal1()).setLowLimit(-7.0).add();
+        SecurityAnalysisResult result3 = runSecurityAnalysis(network, contingencies, Collections.emptyList(), new SecurityAnalysisParameters());
+        assertTrue(result3.getPostContingencyResults().get(0).getLimitViolationsResult().getLimitViolations().isEmpty());
     }
 }
