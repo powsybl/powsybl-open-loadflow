211eaa584f011c8d696b0308371cf0e91d3086fe
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-10-16 15:36:37 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/outerloop/AutomationSystemOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/ac/outerloop/AutomationSystemOuterLoop.java
@@ -8,12 +8,16 @@ package com.powsybl.openloadflow.ac.outerloop;
 
 import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.openloadflow.ac.AcOuterLoopContext;
+import com.powsybl.openloadflow.graph.GraphConnectivity;
 import com.powsybl.openloadflow.lf.outerloop.OuterLoopStatus;
-import com.powsybl.openloadflow.network.LfBranch;
-import com.powsybl.openloadflow.network.LfOverloadManagementSystem;
+import com.powsybl.openloadflow.network.*;
+import com.powsybl.openloadflow.network.impl.LfSwitch;
 import com.powsybl.openloadflow.util.PerUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -32,21 +36,39 @@ public class AutomationSystemOuterLoop implements AcOuterLoop {
     @Override
     public OuterLoopStatus check(AcOuterLoopContext context, Reporter reporter) {
         OuterLoopStatus status = OuterLoopStatus.STABLE;
-        for (LfOverloadManagementSystem system : context.getNetwork().getOverloadManagementSystems()) {
-            LfBranch branch = system.getBranchToMonitor();
-            if (branch.isConnectedAtBothSides()) {
-                double i1 = branch.getI1().eval();
+        LfNetwork network = context.getNetwork();
+        Set<LfBranch> branchesToOpen = new HashSet<>();
+        Set<LfBranch> branchesToClose = new HashSet<>();
+        for (LfOverloadManagementSystem system : network.getOverloadManagementSystems()) {
+            LfBranch branchToMonitor = system.getBranchToMonitor();
+            if (branchToMonitor.isConnectedAtBothSides()) {
+                double i1 = branchToMonitor.getI1().eval();
                 double threshold = system.getThreshold();
                 if (i1 > threshold) {
-                    double ib = PerUnit.ib(branch.getBus1().getNominalV());
-                    LOGGER.debug("Line '{}' is overloaded ({} A > {} A), {} switch '{}'",
-                            branch.getId(), i1 * ib, threshold * ib, system.isSwitchOpen() ? "open" : "close",
-                            system.getSwitchToOperate().getId());
-                    system.getSwitchToOperate().setDisabled(system.isSwitchOpen());
-                    status = OuterLoopStatus.UNSTABLE;
+                    double ib = PerUnit.ib(branchToMonitor.getBus1().getNominalV());
+                    LfSwitch switchToOperate = system.getSwitchToOperate();
+                    if (system.isSwitchOpen() && switchToOperate.isConnectedAtBothSides()) {
+                        LOGGER.debug("Line '{}' is overloaded ({} A > {} A), open switch '{}'",
+                                branchToMonitor.getId(), i1 * ib, threshold * ib, switchToOperate.getId());
+                        branchesToOpen.add(switchToOperate);
+                    } else if (!system.isSwitchOpen() && !switchToOperate.isConnectedAtBothSides()) {
+                        LOGGER.debug("Line '{}' is overloaded ({} A > {} A), close switch '{}'",
+                                branchToMonitor.getId(), i1 * ib, threshold * ib, switchToOperate.getId());
+                        branchesToClose.add(switchToOperate);
+                    }
                 }
             }
         }
+
+        if (branchesToOpen.size() + branchesToClose.size() > 0) {
+            GraphConnectivity<LfBus, LfBranch> connectivity = network.getConnectivity();
+            connectivity.startTemporaryChanges();
+            branchesToOpen.forEach(connectivity::removeEdge);
+            branchesToClose.forEach(branch -> connectivity.addEdge(branch.getBus1(), branch.getBus2(), branch));
+            LfAction.updateBusesAndBranchStatus(connectivity);
+            status = OuterLoopStatus.UNSTABLE;
+        }
+
         return status;
     }
 }
