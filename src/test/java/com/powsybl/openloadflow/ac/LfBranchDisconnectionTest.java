ec00ea72b8240ce59b5956edad072dd17b5835cb
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-11-27 19:58:46 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/test/java/com/powsybl/openloadflow/ac/LfBranchDisconnectionTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/LfBranchDisconnectionTest.java
@@ -9,19 +9,13 @@ package com.powsybl.openloadflow.ac;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import com.powsybl.math.matrix.DenseMatrixFactory;
-import com.powsybl.openloadflow.OpenLoadFlowParameters;
-import com.powsybl.openloadflow.ac.equations.AcEquationSystemCreationParameters;
-import com.powsybl.openloadflow.ac.nr.NewtonRaphsonParameters;
-import com.powsybl.openloadflow.ac.nr.NewtonRaphsonStatus;
+import com.powsybl.openloadflow.ac.solver.AcSolverStatus;
 import com.powsybl.openloadflow.network.EurostagFactory;
 import com.powsybl.openloadflow.network.LfBranch;
 import com.powsybl.openloadflow.network.LfNetwork;
 import com.powsybl.openloadflow.network.LfNetworkParameters;
 import com.powsybl.openloadflow.network.impl.Networks;
-import com.powsybl.openloadflow.network.util.UniformValueVoltageInitializer;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -41,19 +35,12 @@ class LfBranchDisconnectionTest {
         lfl1.setDisconnectionAllowedSide1(true);
         lfl1.setDisconnectionAllowedSide2(true);
 
-        AcLoadFlowParameters acParameters = new AcLoadFlowParameters(new LfNetworkParameters(),
-                                                                     new AcEquationSystemCreationParameters(),
-                                                                     new NewtonRaphsonParameters(),
-                                                                     Collections.emptyList(),
-                                                                     AcLoadFlowParameters.DEFAULT_MAX_OUTER_LOOP_ITERATIONS,
-                                                                     new DenseMatrixFactory(),
-                                                                     new UniformValueVoltageInitializer(),
-                                                                     false,
-                                                                     OpenLoadFlowParameters.SlackDistributionFailureBehavior.LEAVE_ON_SLACK_BUS);
+        AcLoadFlowParameters acParameters = new AcLoadFlowParameters()
+                .setMatrixFactory(new DenseMatrixFactory());
         try (var context = new AcLoadFlowContext(lfNetwork, acParameters)) {
             AcLoadFlowResult result = new AcloadFlowEngine(context)
                     .run();
-            assertEquals(NewtonRaphsonStatus.CONVERGED, result.getNewtonRaphsonStatus());
+            assertEquals(AcSolverStatus.CONVERGED, result.getSolverStatus());
             assertEquals(3.02444, lfl1.getP1().eval(), DELTA);
             assertEquals(0.98739, lfl1.getQ1().eval(), DELTA);
             assertEquals(-3.00434, lfl1.getP2().eval(), DELTA);
@@ -62,7 +49,7 @@ class LfBranchDisconnectionTest {
             lfl1.setConnectedSide1(false);
             result = new AcloadFlowEngine(context)
                     .run();
-            assertEquals(NewtonRaphsonStatus.CONVERGED, result.getNewtonRaphsonStatus());
+            assertEquals(AcSolverStatus.CONVERGED, result.getSolverStatus());
             assertTrue(Double.isNaN(lfl1.getP1().eval()));
             assertTrue(Double.isNaN(lfl1.getQ1().eval()));
             assertEquals(1.752e-4, lfl1.getP2().eval(), DELTA);
@@ -71,7 +58,7 @@ class LfBranchDisconnectionTest {
             lfl1.setConnectedSide1(true);
             result = new AcloadFlowEngine(context)
                     .run();
-            assertEquals(NewtonRaphsonStatus.CONVERGED, result.getNewtonRaphsonStatus());
+            assertEquals(AcSolverStatus.CONVERGED, result.getSolverStatus());
             assertEquals(3.02444, lfl1.getP1().eval(), DELTA);
             assertEquals(0.98739, lfl1.getQ1().eval(), DELTA);
             assertEquals(-3.00434, lfl1.getP2().eval(), DELTA);
@@ -80,7 +67,7 @@ class LfBranchDisconnectionTest {
             lfl1.setConnectedSide2(false);
             result = new AcloadFlowEngine(context)
                     .run();
-            assertEquals(NewtonRaphsonStatus.CONVERGED, result.getNewtonRaphsonStatus());
+            assertEquals(AcSolverStatus.CONVERGED, result.getSolverStatus());
             assertEquals(1.752e-4, lfl1.getP1().eval(), DELTA);
             assertEquals(-0.5811, lfl1.getQ1().eval(), DELTA);
             assertTrue(Double.isNaN(lfl1.getP2().eval()));
@@ -89,7 +76,7 @@ class LfBranchDisconnectionTest {
             lfl1.setConnectedSide2(true);
             result = new AcloadFlowEngine(context)
                     .run();
-            assertEquals(NewtonRaphsonStatus.CONVERGED, result.getNewtonRaphsonStatus());
+            assertEquals(AcSolverStatus.CONVERGED, result.getSolverStatus());
             assertEquals(3.02444, lfl1.getP1().eval(), DELTA);
             assertEquals(0.98739, lfl1.getQ1().eval(), DELTA);
             assertEquals(-3.00434, lfl1.getP2().eval(), DELTA);
@@ -99,7 +86,7 @@ class LfBranchDisconnectionTest {
             lfl1.setConnectedSide2(false);
             result = new AcloadFlowEngine(context)
                     .run();
-            assertEquals(NewtonRaphsonStatus.CONVERGED, result.getNewtonRaphsonStatus());
+            assertEquals(AcSolverStatus.CONVERGED, result.getSolverStatus());
             assertTrue(Double.isNaN(lfl1.getP1().eval()));
             assertTrue(Double.isNaN(lfl1.getQ1().eval()));
             assertTrue(Double.isNaN(lfl1.getP2().eval()));
