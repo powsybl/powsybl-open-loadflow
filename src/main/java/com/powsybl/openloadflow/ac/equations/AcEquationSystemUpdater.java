f76d7f49cd7769831fb7a28436e0aa932d558837
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-11-28 22:18:43 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/AcEquationSystemUpdater.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/AcEquationSystemUpdater.java
@@ -7,6 +7,7 @@
 package com.powsybl.openloadflow.ac.equations;
 
 import com.powsybl.iidm.network.TwoSides;
+import com.powsybl.openloadflow.equations.Equation;
 import com.powsybl.openloadflow.equations.EquationSystem;
 import com.powsybl.openloadflow.lf.AbstractEquationSystemUpdater;
 import com.powsybl.openloadflow.network.*;
@@ -207,8 +208,13 @@ public class AcEquationSystemUpdater extends AbstractEquationSystemUpdater<AcVar
     @Override
     public void onReferenceBusChange(LfBus bus, boolean reference) {
         if (reference) {
-            equationSystem.createEquation(bus, AcEquationType.BUS_TARGET_PHI)
-                    .setActive(true);
+            Equation<AcVariableType, AcEquationType> phiEq = equationSystem.getEquation(bus.getNum(), AcEquationType.BUS_TARGET_PHI).orElse(null);
+            if (phiEq == null) {
+                phiEq = equationSystem.createEquation(bus, AcEquationType.BUS_TARGET_PHI)
+                        .addTerm(equationSystem.getVariable(bus.getNum(), AcVariableType.BUS_PHI)
+                                .createTerm());
+            }
+            phiEq.setActive(true);
         } else {
             equationSystem.getEquation(bus.getNum(), AcEquationType.BUS_TARGET_PHI)
                     .orElseThrow()
