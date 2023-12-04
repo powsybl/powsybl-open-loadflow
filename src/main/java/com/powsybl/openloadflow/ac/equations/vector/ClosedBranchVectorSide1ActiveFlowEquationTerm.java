6ed47b6ea3674561bd15edcd73e982d3d5060d4a
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-12-04 20:26:33 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/vector/ClosedBranchVectorSide1ActiveFlowEquationTerm.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/vector/ClosedBranchVectorSide1ActiveFlowEquationTerm.java
@@ -7,14 +7,12 @@
 package com.powsybl.openloadflow.ac.equations.vector;
 
 import com.powsybl.openloadflow.ac.equations.AcVariableType;
+import com.powsybl.openloadflow.ac.equations.ClosedBranchSide1ActiveFlowEquationTerm;
 import com.powsybl.openloadflow.equations.Variable;
 import com.powsybl.openloadflow.equations.VariableSet;
 import com.powsybl.openloadflow.util.Fortescue;
-import net.jafama.FastMath;
 
 import java.util.Objects;
-
-import static com.powsybl.openloadflow.ac.equations.ClosedBranchSide1ActiveFlowEquationTerm.*;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -27,29 +25,12 @@ public class ClosedBranchVectorSide1ActiveFlowEquationTerm extends AbstractClose
         super(branchVector, branchNum, bus1Num, bus2Num, variableSet, deriveA1, deriveR1, Fortescue.SequenceType.POSITIVE);
     }
 
-    public ClosedBranchVectorSide1ActiveFlowEquationTerm(AcBranchVector branchVector, int branchNum, int bus1Num, int bus2Num,
-                                                         VariableSet<AcVariableType> variableSet, boolean deriveA1, boolean deriveR1,
-                                                         Fortescue.SequenceType sequenceType) {
-        super(branchVector, branchNum, bus1Num, bus2Num, variableSet, deriveA1, deriveR1, sequenceType);
-    }
-
     protected double calculateSensi(double dph1, double dph2, double dv1, double dv2, double da1, double dr1) {
         double y = branchVector.y[num];
         double ksi = branchVector.ksi[num];
         double g1 = branchVector.g1[num];
-        double v1 = v1();
-        double r1 = r1();
-        double v2 = v2();
-        double theta = theta1(ksi, ph1(), a1(), ph2());
-        double cosTheta = FastMath.cos(theta);
-        double sinTheta = FastMath.sin(theta);
-        double sinKsi = FastMath.sin(ksi);
-        return dp1dph1(y, v1, r1, v2, cosTheta) * dph1
-                + dp1dph2(y, v1, r1, v2, cosTheta) * dph2
-                + dp1dv1(y, sinKsi, g1, v1, r1, v2, sinTheta) * dv1
-                + dp1dv2(y, v1, r1, sinTheta) * dv2
-                + dp1da1(y, v1, r1, v2, cosTheta) * da1
-                + dp1dr1(y, sinKsi, g1, v1, r1, v2, sinTheta) * dr1;
+        return ClosedBranchSide1ActiveFlowEquationTerm.calculateSensi(g1, y, ksi, v1(), ph1(), a1(), r1(), v2(), ph2(),
+                dph1, dph2, dv1, dv2, da1, dr1);
     }
 
     @Override
