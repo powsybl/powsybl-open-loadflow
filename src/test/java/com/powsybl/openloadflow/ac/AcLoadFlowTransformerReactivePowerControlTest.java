0e36f089bd723eb844598ffc5db4586d1a04bfea
parvy <pierre.arvy@artelys.com>
2023-12-14 11:33:30 +0100

    refactor.
    Signed-off-by: parvy <pierre.arvy@artelys.com>

--- src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowTransformerReactivePowerControlTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowTransformerReactivePowerControlTest.java
@@ -1,8 +1,9 @@
 /**
- * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 
 package com.powsybl.openloadflow.ac;
@@ -348,7 +349,7 @@ class AcLoadFlowTransformerReactivePowerControlTest {
     }
 
     @Test
-    void transformerReactivePowerControlNonImpedantControlled() {
+    void transformerReactivePowerControlOnNonImpedantBranch() {
         selectNetwork(VoltageControlNetworkFactory.createNetworkWithT2wt());
 
         parameters.setTransformerReactivePowerControlOn(true);
