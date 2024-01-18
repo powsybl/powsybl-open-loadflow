5894e2463329a71351a7ba5bdc9624879a8460ee
VIDAL Didier (Externe) <didier.vidal_externe@rte-france.com>
2024-01-18 09:21:15 +0100

    current state of the fix. Not workign for security analysis

--- src/main/java/com/powsybl/openloadflow/network/impl/LfLoadImpl.java
+++ src/main/java/com/powsybl/openloadflow/network/impl/LfLoadImpl.java
@@ -102,7 +102,7 @@ public class LfLoadImpl extends AbstractLfInjection implements LfLoad {
     void add(LccConverterStation lccCs, LfNetworkParameters parameters) {
         // note that LCC converter station are out of the slack distribution.
         lccCsRefs.add(Ref.create(lccCs, parameters.isCacheEnabled()));
-        double lccTargetP = HvdcUtils.getConverterStationTargetP(lccCs);
+        double lccTargetP = LfHvdc.getActualTargetP(lccCs);
         this.targetP += lccTargetP / PerUnit.SB;
         initialTargetP += lccTargetP / PerUnit.SB;
         targetQ += HvdcUtils.getLccConverterStationLoadTargetQ(lccCs) / PerUnit.SB;
@@ -209,7 +209,8 @@ public class LfLoadImpl extends AbstractLfInjection implements LfLoad {
         // update lcc converter station power
         for (Ref<LccConverterStation> lccCsRef : lccCsRefs) {
             LccConverterStation lccCs = lccCsRef.get();
-            double pCs = HvdcUtils.getConverterStationTargetP(lccCs); // A LCC station has active losses.
+            // TODO: Update pCs with dynamic load model taking into account disconnection by contingencies
+            double pCs = LfHvdc.getActualTargetP(lccCs); // A LCC station has active losses.
             double qCs = HvdcUtils.getLccConverterStationLoadTargetQ(lccCs); // A LCC station always consumes reactive power.
             lccCs.getTerminal()
                     .setP(pCs)
