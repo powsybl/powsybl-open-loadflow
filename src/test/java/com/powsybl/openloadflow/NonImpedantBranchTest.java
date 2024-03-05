59ca58d0cb9483e62a09cee3b262af248ad21746
Anne Tilloy <48123713+annetill@users.noreply.github.com>
2024-03-05 10:48:37 +0100

    Fix terminals connection action (#983)
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/NonImpedantBranchTest.java
+++ src/test/java/com/powsybl/openloadflow/NonImpedantBranchTest.java
@@ -20,6 +20,7 @@ import com.powsybl.math.matrix.DenseMatrixFactory;
 import com.powsybl.openloadflow.graph.EvenShiloachGraphDecrementalConnectivityFactory;
 import com.powsybl.openloadflow.network.AbstractLoadFlowNetworkFactory;
 import com.powsybl.openloadflow.network.SlackBusSelectionMode;
+import com.powsybl.openloadflow.network.ZeroImpedanceNetworkFactory;
 import com.powsybl.openloadflow.network.impl.OlfBranchResult;
 import com.powsybl.openloadflow.sa.OpenSecurityAnalysisParameters;
 import com.powsybl.openloadflow.sa.OpenSecurityAnalysisProvider;
@@ -380,46 +381,8 @@ class NonImpedantBranchTest extends AbstractLoadFlowNetworkFactory {
      */
     @Test
     void securityAnalysisNotSameNumberOfVariablesAndEquationsIssueTest() {
-        Network network = Network.create("test", "code");
-        Bus b0 = createBus(network, "s", "b0");
-        Bus b1 = createBus(network, "s", "b1");
-        Bus b2 = createBus(network, "s", "b2");
-        Bus b3 = createBus(network, "s", "b3");
-        Bus b4 = createBus(network, "s", "b4");
-        Bus b5 = createBus(network, "s", "b5");
-        Generator g0 = createGenerator(b0, "g0", 2, 1); // 1 kV
-        createGenerator(b4, "g4", 2, 1.15); // 1.15 kV
-        createLoad(b5, "ld5", 4);
-        Line l01 = createLine(network, b0, b1, "l01", 0.1);
-        createLine(network, b1, b2, "l12", 0.0);
-        createLine(network, b2, b3, "l23", 0.0);
-        createLine(network, b1, b5, "l15", 0.1);
-        createLine(network, b5, b3, "l53", 0.1);
-        g0.setRegulatingTerminal(l01.getTerminal2()); // remote
-        TwoWindingsTransformer t34 = createTransformer(network, "s", b3, b4, "tr34", 0.15, 1);
-        t34.newRatioTapChanger()
-                .beginStep()
-                    .setRho(0.9)
-                .endStep()
-                .beginStep()
-                    .setRho(1)
-                .endStep()
-                .beginStep()
-                    .setRho(1.1)
-                .endStep()
-                .beginStep()
-                    .setRho(1.2)
-                .endStep()
-                .setTapPosition(1)
-                .setLoadTapChangingCapabilities(true)
-                .setRegulating(true)
-                .setTargetV(1.1)
-                .setRegulationTerminal(t34.getTerminal1())
-                .setTargetDeadband(0.01)
-                .add();
-
+        Network network = ZeroImpedanceNetworkFactory.createWithVoltageControl();
         List<Contingency> contingencies = List.of(new Contingency("contingency", List.of(new BranchContingency("l01"))));
-
         LoadFlowParameters loadFlowParameters = new LoadFlowParameters()
                 .setDistributedSlack(false)
                 .setTransformerVoltageControlOn(true);
