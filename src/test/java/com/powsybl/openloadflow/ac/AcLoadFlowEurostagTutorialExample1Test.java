b2677252fc1a0440cbf279ed49504c9ed9bcbf78
Anne Tilloy <anne.tilloy@rte-france.com>
2024-03-13 15:39:15 +0100

    Fix proposal and adapt unit test.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowEurostagTutorialExample1Test.java
+++ src/test/java/com/powsybl/openloadflow/ac/AcLoadFlowEurostagTutorialExample1Test.java
@@ -468,9 +468,23 @@ class AcLoadFlowEurostagTutorialExample1Test {
 
     @Test
     void testWriteReadSlackBus() {
-        Network network = EurostagTutorialExample1Factory.create();
+        Network network = EurostagFactory.fix(EurostagTutorialExample1Factory.create());
         network.getVariantManager().cloneVariant(network.getVariantManager().getWorkingVariantId(), "newVariant");
-        LoadFlow.run(network, new LoadFlowParameters().setWriteSlackBus(true));
-        LoadFlow.run(network, "newVariant", LocalComputationManager.getDefault(), new LoadFlowParameters());
+        LoadFlowParameters parameters = new LoadFlowParameters();
+        parameters.setWriteSlackBus(true);
+        OpenLoadFlowParameters openLoadFlowParameters =
+                OpenLoadFlowParameters.create(parameters).setSlackBusSelectionMode(SlackBusSelectionMode.FIRST);
+        LoadFlowResult result = LoadFlow.run(network, parameters);
+        assertTrue(result.isFullyConverged());
+        SlackTerminal slackTerminal = network.getVoltageLevel("VLGEN").getExtension(SlackTerminal.class);
+        assertNotNull(slackTerminal);
+        assertNotNull(slackTerminal.getTerminal());
+        openLoadFlowParameters.setSlackBusSelectionMode(SlackBusSelectionMode.MOST_MESHED);
+        LoadFlowResult result2 = LoadFlow.run(network, "newVariant", LocalComputationManager.getDefault(), parameters);
+        assertTrue(result2.isFullyConverged());
+        assertEquals("VLHV1_0", result2.getComponentResults().get(0).getSlackBusResults().get(0).getId());
+        LoadFlowResult result3 = LoadFlow.run(network, VariantManagerConstants.INITIAL_VARIANT_ID, LocalComputationManager.getDefault(), parameters);
+        assertTrue(result3.isFullyConverged());
+        assertEquals("VLGEN_0", result3.getComponentResults().get(0).getSlackBusResults().get(0).getId());
     }
 }
