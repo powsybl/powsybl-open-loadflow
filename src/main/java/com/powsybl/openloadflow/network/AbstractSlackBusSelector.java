ac678e31232b1cef88be00c6b6528f42bde2efa9
vmouradian <valentin.mouradian@artelys.com>
2024-03-14 17:40:43 +0100

    draft of the fix
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/main/java/com/powsybl/openloadflow/network/AbstractSlackBusSelector.java
+++ src/main/java/com/powsybl/openloadflow/network/AbstractSlackBusSelector.java
@@ -8,8 +8,11 @@ package com.powsybl.openloadflow.network;
 
 import com.powsybl.iidm.network.Country;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Predicate;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -25,5 +28,19 @@ public abstract class AbstractSlackBusSelector implements SlackBusSelector {
     protected boolean filterByCountry(LfBus bus) {
         return countries.isEmpty()
                 || bus.getCountry().map(countries::contains).orElse(false);
+    }
+
+    // TODO see for which other selector this could be used
+    // TODO maybe it will be even better to distinct by ZeroImpedanceNetwork
+    protected static Predicate<LfBus> distinctByNonImpedantBranch() {
+        Set<LfBranch> visitedNonImpedantBranches = new HashSet<>();
+        return b -> {
+            List<LfBranch> nonImpedantBranches = b.getBranches().stream().filter(branch -> branch.isZeroImpedance(LoadFlowModel.DC)).toList();
+            if (visitedNonImpedantBranches.stream().anyMatch(nonImpedantBranches::contains)) {
+                return false;
+            }
+            visitedNonImpedantBranches.addAll(nonImpedantBranches);
+            return true;
+        };
     }
 }
