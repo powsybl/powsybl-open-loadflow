8f4cd8b21ab4ded38af5c824fa16964398d3f8d9
Olivier Perrin <olivier.perrin@rte-france.com>
2024-04-12 15:56:02 +0200

    Support empty network element criteria case
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/sa/LimitReductionManager.java
+++ src/main/java/com/powsybl/openloadflow/sa/LimitReductionManager.java
@@ -7,18 +7,25 @@
 package com.powsybl.openloadflow.sa;
 
 import com.powsybl.contingency.ContingencyContextType;
+import com.powsybl.iidm.criteria.AtLeastOneNominalVoltageCriterion;
 import com.powsybl.iidm.criteria.IdentifiableCriterion;
-import com.powsybl.iidm.criteria.NetworkElementCriterion;
-import com.powsybl.iidm.criteria.duration.*;
+import com.powsybl.iidm.criteria.VoltageInterval;
+import com.powsybl.iidm.criteria.duration.AllTemporaryDurationCriterion;
+import com.powsybl.iidm.criteria.duration.EqualityTemporaryDurationCriterion;
+import com.powsybl.iidm.criteria.duration.IntervalTemporaryDurationCriterion;
+import com.powsybl.iidm.criteria.duration.LimitDurationCriterion;
 import com.powsybl.iidm.network.LimitType;
 import com.powsybl.security.limitreduction.LimitReduction;
+import org.apache.commons.lang3.DoubleRange;
 import org.apache.commons.lang3.Range;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -80,39 +87,41 @@ public class LimitReductionManager {
 
     public static LimitReductionManager create(List<LimitReduction> limitReductions) {
         LimitReductionManager limitReductionManager = new LimitReductionManager();
-        Range<Double> nominalVoltageRange;
-        Range<Integer> acceptableDurationRange = null; // nothing asked
+        Range<Integer> acceptableDurationRange;
         boolean permanent = false;
         for (LimitReduction limitReduction : limitReductions) {
             if (isSupported(limitReduction)) {
-                if (limitReduction.getNetworkElementCriteria().isEmpty()) {
-                    // nothing todo
-                } else {
-                    for (NetworkElementCriterion networkElementCriterion : limitReduction.getNetworkElementCriteria()) {
-                        IdentifiableCriterion identifiableCriterion = (IdentifiableCriterion) networkElementCriterion;
-                        nominalVoltageRange = identifiableCriterion.getNominalVoltageCriterion().getVoltageInterval().asRange();
-                        if (limitReduction.getDurationCriteria().isEmpty()) {
-                            permanent = true;
-                        } else { // size 1 or 2 only (when 2, they are not of the same type).
-                            for (LimitDurationCriterion limitDurationCriterion : limitReduction.getDurationCriteria()) {
-                                switch (limitDurationCriterion.getType()) {
-                                    case PERMANENT -> permanent = true;
-                                    case TEMPORARY -> {
-                                        if (limitDurationCriterion instanceof AllTemporaryDurationCriterion) {
-                                            acceptableDurationRange = Range.of(0, Integer.MAX_VALUE);
-                                        } else if (limitDurationCriterion instanceof EqualityTemporaryDurationCriterion equalityTemporaryDurationCriterion) {
-                                            acceptableDurationRange = Range.of(equalityTemporaryDurationCriterion.getDurationEqualityValue(),
-                                                    equalityTemporaryDurationCriterion.getDurationEqualityValue());
-                                        } else { // intervalTemporaryDurationCriterion
-                                            IntervalTemporaryDurationCriterion intervalTemporaryDurationCriterion = (IntervalTemporaryDurationCriterion) limitDurationCriterion;
-                                            acceptableDurationRange = intervalTemporaryDurationCriterion.asRange();
-                                        }
+                Collection<Range<Double>> nominalVoltageRanges = limitReduction.getNetworkElementCriteria().isEmpty() ?
+                        List.of(DoubleRange.of(0, Double.MAX_VALUE)) :
+                        limitReduction.getNetworkElementCriteria().stream().map(IdentifiableCriterion.class::cast)
+                                .map(IdentifiableCriterion::getNominalVoltageCriterion)
+                                .map(AtLeastOneNominalVoltageCriterion::getVoltageInterval)
+                                .map(VoltageInterval::asRange)
+                                .collect(Collectors.toSet());
+
+                for (Range<Double> nominalVoltageRange : nominalVoltageRanges) {
+                    acceptableDurationRange = null;
+                    if (limitReduction.getDurationCriteria().isEmpty()) {
+                        permanent = true;
+                    } else { // size 1 or 2 only (when 2, they are not of the same type).
+                        for (LimitDurationCriterion limitDurationCriterion : limitReduction.getDurationCriteria()) {
+                            switch (limitDurationCriterion.getType()) {
+                                case PERMANENT -> permanent = true;
+                                case TEMPORARY -> {
+                                    if (limitDurationCriterion instanceof AllTemporaryDurationCriterion) {
+                                        acceptableDurationRange = Range.of(0, Integer.MAX_VALUE);
+                                    } else if (limitDurationCriterion instanceof EqualityTemporaryDurationCriterion equalityTemporaryDurationCriterion) {
+                                        acceptableDurationRange = Range.of(equalityTemporaryDurationCriterion.getDurationEqualityValue(),
+                                                equalityTemporaryDurationCriterion.getDurationEqualityValue());
+                                    } else { // intervalTemporaryDurationCriterion
+                                        IntervalTemporaryDurationCriterion intervalTemporaryDurationCriterion = (IntervalTemporaryDurationCriterion) limitDurationCriterion;
+                                        acceptableDurationRange = intervalTemporaryDurationCriterion.asRange();
                                     }
                                 }
                             }
                         }
-                        limitReductionManager.addTerminalLimitReduction(new TerminalLimitReduction(nominalVoltageRange, permanent, acceptableDurationRange, limitReduction.getValue()));
                     }
+                    limitReductionManager.addTerminalLimitReduction(new TerminalLimitReduction(nominalVoltageRange, permanent, acceptableDurationRange, limitReduction.getValue()));
                 }
             }
         }
