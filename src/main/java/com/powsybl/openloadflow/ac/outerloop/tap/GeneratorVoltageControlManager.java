b5e623aecc0ba6b9197006af5a13f0defe25d2c8
Anne Tilloy <anne.tilloy@rte-france.com>
2024-06-21 15:47:22 +0200

    Add documentation + add load tap changing capabilities in criteria.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/outerloop/tap/GeneratorVoltageControlManager.java
+++ src/main/java/com/powsybl/openloadflow/ac/outerloop/tap/GeneratorVoltageControlManager.java
@@ -34,14 +34,14 @@ public class GeneratorVoltageControlManager {
         for (LfBus bus : network.getBuses()) {
             if (!bus.isDisabled()
                     && bus.isTransformerVoltageControlled()
-                    && isVoltageControllingTransformerChangingVoltageLevel(bus.getTransformerVoltageControl().orElse(null))) {
+                    && isTransformerVoltageControlsValidForMaxControlledNominalVoltageCalculation(bus.getTransformerVoltageControl().orElse(null))) {
                 defaultMinNominalVoltage = Math.max(defaultMinNominalVoltage, bus.getNominalV());
             }
         }
         return defaultMinNominalVoltage;
     }
 
-    private static boolean isVoltageControllingTransformerChangingVoltageLevel(TransformerVoltageControl transformerVoltageControl) {
+    private static boolean isTransformerVoltageControlsValidForMaxControlledNominalVoltageCalculation(TransformerVoltageControl transformerVoltageControl) {
         // are removed from this automatic algorithm the transformer voltage control that are between two nominal
         // voltages equivalents.
         if (transformerVoltageControl != null) {
@@ -60,13 +60,14 @@ public class GeneratorVoltageControlManager {
      * Disables the voltage control of generators if controlled bus nominal voltage is under the limit.
      */
     public void disableGeneratorVoltageControlsUnderMaxControlledNominalVoltage(LfNetwork network) {
-        disabledControllerBuses.clear(); // (in case someone wants to call this multiple time in instance lifecycle
-                                         // although this class as not designed nor tested with this use case in mind)
+        // Clear in case someone wants to call this multiple time in instance lifecycle
+        // although this class as not designed nor tested with this use case in mind.
+        disabledControllerBuses.clear();
         for (LfBus bus : network.getControlledBuses(VoltageControl.Type.GENERATOR)) {
             if (bus.getNominalV() < minNominalVoltageLimit) {
                 var voltageControl = bus.getGeneratorVoltageControl().orElseThrow();
                 for (LfBus controllerBus : voltageControl.getMergedControllerElements()) {
-                    if (controllerBus.isGeneratorVoltageControlEnabled() && !isBusBehindVeryHighVoltageTransfo(controllerBus, minNominalVoltageLimit)) {
+                    if (controllerBus.isGeneratorVoltageControlEnabled() && !hasStepUpTransformers(controllerBus, minNominalVoltageLimit)) {
                         controllerBus.setGenerationTargetQ(controllerBus.getQ().eval());
                         controllerBus.setGeneratorVoltageControlEnabled(false);
                         disabledControllerBuses.add(controllerBus);
@@ -86,32 +87,27 @@ public class GeneratorVoltageControlManager {
         }
     }
 
-    private boolean isBusBehindVeryHighVoltageTransfo(LfBus bus, double limit) {
-        // The criteria detects the following cases at this point
-        // The bus is connected to a VSC station
-        // or
-        // the bus is connected to a generator and to transfomers that all step up to a tension above nominal voltage
-        //
-
-        if (bus.getGenerators().isEmpty()) {
-            return false;
-        }
-
-        // Always keep VSC stations
+    /**
+     * True if a controller bus:
+     * - has a VSC converter station;
+     * - has step-up transformers converting voltage to a bus with nominal voltage higher than the voltage limit. A step-up
+     * transformer has no tap changing capabilities.
+     */
+    private boolean hasStepUpTransformers(LfBus bus, double limit) {
         if (bus.getGenerators().stream().anyMatch(LfVscConverterStation.class::isInstance)) {
             return true;
         }
+        if (bus.getBranches().stream().anyMatch(b -> b.getVoltageControl().isPresent())) {
+            return false;
+        }
 
         double startingNominalVoltage = bus.getNominalV();
-
         double minConnectedVoltageLevel = bus.getBranches().stream()
                 .filter(b -> b.isConnectedAtBothSides())
                 .mapToDouble(b -> Math.max(b.getBus1().getNominalV(), b.getBus2().getNominalV()))
                 .min()
                 .orElse(-1);
-
-        // ALl branches should be step up transformers arriving to a voltage higher than limit
+        // All branches should be step-up transformers arriving to a voltage higher than limit
         return minConnectedVoltageLevel > startingNominalVoltage && minConnectedVoltageLevel > limit;
-
     }
 }
