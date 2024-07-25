d7456e742ad0753beb3541d68cd5eb10dd36abdd
vmouradian <valentin.mouradian@artelys.com>
2024-07-25 10:15:01 +0200

    Fix copyrights and javadoc
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/main/java/com/powsybl/openloadflow/network/impl/LfAreaImpl.java
+++ src/main/java/com/powsybl/openloadflow/network/impl/LfAreaImpl.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.openloadflow.network.impl;
 
 import com.powsybl.iidm.network.*;
@@ -9,6 +16,9 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Supplier;
 
+/**
+ * @author Valentin Mouradian {@literal <valentin.mouradian at artelys.com>}
+ */
 public class LfAreaImpl extends AbstractPropertyBag implements LfArea {
 
     private final LfNetwork network;
--- src/test/java/com/powsybl/openloadflow/ac/AreaInterchangeControlTests.java
+++ src/test/java/com/powsybl/openloadflow/ac/AreaInterchangeControlTests.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.openloadflow.ac;
 
 import com.powsybl.iidm.network.Area;
@@ -16,6 +23,9 @@ import java.util.concurrent.CompletionException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Valentin Mouradian {@literal <valentin.mouradian at artelys.com>}
+ */
 class AreaInterchangeControlTests {
 
     private LoadFlow.Runner loadFlowRunner;
