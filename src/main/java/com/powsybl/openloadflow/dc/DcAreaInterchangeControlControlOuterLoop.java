3c8d0e39574d4bd03eb4732721782c7acb2f02ee
vmouradian <valentin.mouradian@artelys.com>
2024-10-14 10:51:18 +0200

    clean
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/main/java/com/powsybl/openloadflow/dc/DcAreaInterchangeControlControlOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/dc/DcAreaInterchangeControlControlOuterLoop.java
@@ -42,41 +42,6 @@ public class DcAreaInterchangeControlControlOuterLoop extends AbstractAreaInterc
         return check(context, reportNode, slackBusActivePowerMismatch);
     }
 
-//    @Override
-//    protected OuterLoopResult buildOuterLoopResult(Map<String, Pair<Set<LfBus>, Double>> areas, Map<String, ActivePowerDistribution.Result> resultByArea, ReportNode reportNode, DcOuterLoopContext context) {
-//        Map<String, Double> remainingMismatchByArea = resultByArea.entrySet().stream()
-//                .filter(e -> !lessThanInterchangeMaxMismatch(e.getValue().remainingMismatch()))
-//                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().remainingMismatch()));
-//        double totalDistributedActivePower = resultByArea.entrySet().stream().mapToDouble(e -> areas.get(e.getKey()).getRight() - e.getValue().remainingMismatch()).sum();
-//        boolean movedBuses = resultByArea.values().stream().map(ActivePowerDistribution.Result::movedBuses).reduce(false, (a, b) -> a || b);
-//        Map<String, Integer> iterationsByArea = resultByArea.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().iteration()));
-//
-//        ReportNode iterationReportNode = Reports.createOuterLoopIterationReporter(reportNode, 23); // FIXME don't know what to do here
-//        AreaInterchangeControlContextData contextData = (AreaInterchangeControlContextData) context.getData();
-//        contextData.addDistributedActivePower(totalDistributedActivePower);
-//        if (!remainingMismatchByArea.isEmpty()) {
-//            LOGGER.error(FAILED_TO_DISTRIBUTE_INTERCHANGE_ACTIVE_POWER_MISMATCH);
-//            ReportNode failureReportNode = Reports.reportAreaInterchangeControlDistributionFailure(iterationReportNode);
-//            remainingMismatchByArea.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(entry -> {
-//                LOGGER.error("Remaining mismatch for Area {}: {} MW", entry.getKey(), entry.getValue() * PerUnit.SB);
-//                Reports.reportAreaInterchangeControlAreaMismatch(failureReportNode, entry.getKey(), entry.getValue() * PerUnit.SB);
-//            }
-//            );
-//            return handleDistributionFailure(context, contextData, movedBuses, totalDistributedActivePower, Double.NaN, FAILED_TO_DISTRIBUTE_INTERCHANGE_ACTIVE_POWER_MISMATCH);
-//        } else {
-//            if (movedBuses) {
-//                areas.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(entry -> {
-//                    LOGGER.info("Area {} interchange mismatch ({} MW) distributed in {} distribution iteration(s)", entry.getKey(), entry.getValue().getValue() * PerUnit.SB, iterationsByArea.get(entry.getKey()));
-//                    Reports.reportAreaInterchangeControlAreaDistributionSuccess(iterationReportNode, entry.getKey(), entry.getValue().getValue() * PerUnit.SB, iterationsByArea.get(entry.getKey()));
-//                }
-//                );
-//                return new OuterLoopResult(this, OuterLoopStatus.UNSTABLE);
-//            } else {
-//                return new OuterLoopResult(this, OuterLoopStatus.STABLE);
-//            }
-//        }
-//    }
-
     @Override
     public OpenLoadFlowParameters.SlackDistributionFailureBehavior getSlackDistributionFailureBehavior(DcOuterLoopContext context) {
         return OpenLoadFlowParameters.SlackDistributionFailureBehavior.LEAVE_ON_SLACK_BUS;
