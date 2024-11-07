cb7f556e9e7bf3ab63c9b507edf0a4cce42f58b9
Didier Vidal <didier.vidal_externe@rte-france.com>
2024-11-07 09:58:23 +0100

    Add copyright and authorship on new file
    Signed-off-by: Didier Vidal <didier.vidal_externe@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/equations/LUDecompositionTest.java
+++ src/test/java/com/powsybl/openloadflow/equations/LUDecompositionTest.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 package com.powsybl.openloadflow.equations;
 
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
@@ -19,6 +27,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * @author Didier Vidal {@literal <didier.vidal_externe at rte-france.com>}
+ */
 class LUDecompositionTest {
 
     class LUDecompositionMockIncrementalFailure implements LUDecomposition {
