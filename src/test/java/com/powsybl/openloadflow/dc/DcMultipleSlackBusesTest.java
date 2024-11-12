d139083460fa932836efc415ee42566967713fef
vmouradian <valentin.mouradian@artelys.com>
2024-11-12 16:42:51 +0100

    copyrigth
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/test/java/com/powsybl/openloadflow/dc/DcMultipleSlackBusesTest.java
+++ src/test/java/com/powsybl/openloadflow/dc/DcMultipleSlackBusesTest.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 package com.powsybl.openloadflow.dc;
 
 import com.powsybl.iidm.network.Bus;
@@ -25,6 +33,9 @@ import static com.powsybl.openloadflow.util.LoadFlowAssert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * @author Valentin Mouradian {@literal <valentin.mouradian at artelys.com>}
+ */
 class DcMultipleSlackBusesTest {
 
     private Network network;
