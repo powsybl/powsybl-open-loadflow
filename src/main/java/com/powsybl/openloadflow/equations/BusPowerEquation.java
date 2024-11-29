ed057dfa13d62dcf3b7e6451e1cca5ecc2bec5f8
vmouradian <valentin.mouradian@artelys.com>
2024-11-29 17:54:26 +0100

    copyright and author
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>
    Signed-off-by: Didier Vidal <didier.vidal_externe@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/equations/BusPowerEquation.java
+++ src/main/java/com/powsybl/openloadflow/equations/BusPowerEquation.java
@@ -1,5 +1,15 @@
+/**
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.openloadflow.equations;
 
+/**
+ * @author Valentin Mouradian {@literal <valentin.mouradian at artelys.com>}
+ */
 public class BusPowerEquation<V extends Enum<V> & Quantity, E extends Enum<E> & Quantity> extends Equation<V, E> {
 
     BusPowerEquation(int elementNum, E type, EquationSystem<V, E> equationSystem) {
