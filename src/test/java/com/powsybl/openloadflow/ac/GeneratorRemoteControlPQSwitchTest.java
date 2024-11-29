cebdeee767f45f0ab2a58f0601870422d66cdd8c
Didier Vidal <didier.vidal_externe@rte-france.com>
2024-11-29 16:34:24 +0100

    cleaning and adding (futile) coverage
    Signed-off-by: Didier Vidal <didier.vidal_externe@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/ac/GeneratorRemoteControlPQSwitchTest.java
+++ src/test/java/com/powsybl/openloadflow/ac/GeneratorRemoteControlPQSwitchTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.openloadflow.ac;
 
 import ch.qos.logback.classic.Level;
@@ -28,6 +35,9 @@ import org.slf4j.LoggerFactory;
 import static org.junit.jupiter.api.Assertions.*;
 import static com.powsybl.openloadflow.util.LoadFlowAssert.*;
 
+/**
+ * @author Didier Vidal {@literal <didier.vidal_externe at rte-france.com>}
+ */
 public class GeneratorRemoteControlPQSwitchTest {
 
     private Network network;
