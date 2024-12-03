dec6e7c92c54aa92b13150136c089c386cc6ce05
vmouradian <valentin.mouradian@artelys.com>
2024-12-03 13:27:39 +0100

    introduce contingency parameters extension
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
+++ src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
@@ -62,7 +62,6 @@ import java.util.stream.Stream;
 import static com.powsybl.openloadflow.util.LoadFlowAssert.*;
 import static java.util.Collections.emptySet;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -3996,5 +3995,18 @@ class OpenSecurityAnalysisTest extends AbstractOpenSecurityAnalysisTest {
         var saResultAll = runSecurityAnalysis(network, Collections.emptyList(), createNetworkMonitors(network), lfParametersAll);
         assertEquals(LoadFlowResult.ComponentResult.Status.CONVERGED, saResultAll.getPreContingencyResult().getStatus());
         assertEquals(6, saResultAll.getPreContingencyResult().getNetworkResult().getBusResults().size()); // 6 buses in total
+    }
+
+    @Test
+    void testContingencyParametersExtension() {
+        Contingency contingency = new Contingency("L2", new BranchContingency("L2"));
+        contingency.addExtension(ContingencyParameters.class, new ContingencyParameters(false, true, LoadFlowParameters.BalanceType.PROPORTIONAL_TO_LOAD));
+
+        ContingencyParameters contingencyParameters = contingency.getExtension(ContingencyParameters.class);
+
+        assertEquals(contingencyParameters, contingency.getExtensionByName("ContingencyParameters"));
+        assertFalse(contingencyParameters.isDistributedSlack());
+        assertTrue(contingencyParameters.isAreaInterchangeControl());
+        assertEquals(LoadFlowParameters.BalanceType.PROPORTIONAL_TO_LOAD, contingencyParameters.getBalanceType());
     }
 }
