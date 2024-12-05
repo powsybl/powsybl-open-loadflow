bc2690400154831c952b1452ba7360d69bdfea63
vmouradian <valentin.mouradian@artelys.com>
2024-12-05 18:30:13 +0100

    add serde (no tests yet)
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

---
+++ src/main/java/com/powsybl/openloadflow/sa/ContingencyParametersJsonSerializer.java
@@ -0,0 +1,58 @@
+package com.powsybl.openloadflow.sa;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.powsybl.commons.extensions.ExtensionJsonSerializer;
+import com.powsybl.commons.json.JsonUtil;
+import com.powsybl.contingency.Contingency;
+
+import java.io.IOException;
+
+public class ContingencyParametersJsonSerializer implements ExtensionJsonSerializer<Contingency, ContingencyParameters> {
+
+    @Override
+    public String getExtensionName() {
+        return "contigency-parameters";
+    }
+
+    @Override
+    public String getCategoryName() {
+        return "contingency";
+    }
+
+    @Override
+    public Class<? super ContingencyParameters> getExtensionClass() {
+        return ContingencyParameters.class;
+    }
+
+    /**
+     * Specifies serialization for our extension: ignore name et extendable
+     */
+    private interface SerializationSpec {
+
+        @JsonIgnore
+        String getName();
+
+        @JsonIgnore
+        Contingency getExtendable();
+    }
+
+    private static ObjectMapper createMapper() {
+        return JsonUtil.createObjectMapper()
+                .addMixIn(ContingencyParameters.class, SerializationSpec.class);
+    }
+
+    @Override
+    public void serialize(ContingencyParameters extension, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        createMapper().writeValue(jsonGenerator, extension);
+    }
+
+    @Override
+    public ContingencyParameters deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        return createMapper().readValue(jsonParser, ContingencyParameters.class);
+    }
+}
