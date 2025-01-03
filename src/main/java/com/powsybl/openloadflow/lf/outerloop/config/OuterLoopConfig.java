430397967e21bc7ee13bd9e655bacd91d5eda8be
vmouradian <valentin.mouradian@artelys.com>
2025-01-03 14:06:10 +0100

    fix copyright/author
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/main/java/com/powsybl/openloadflow/lf/outerloop/config/OuterLoopConfig.java
+++ src/main/java/com/powsybl/openloadflow/lf/outerloop/config/OuterLoopConfig.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -19,6 +20,7 @@ import java.util.ServiceLoader;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
+ * @author Valentin Mouradian {@literal <valentin.mouradian at artelys.com>}
  */
 public interface OuterLoopConfig<O extends OuterLoop<?, ?, ?, ?, ?>> {
     List<O> configure(LoadFlowParameters parameters, OpenLoadFlowParameters parametersExt);
