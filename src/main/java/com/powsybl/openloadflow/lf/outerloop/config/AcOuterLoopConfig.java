b666c0be3d1c60594f39b9ae29cdc01a68ee96a6
vmouradian <valentin.mouradian@artelys.com>
2025-01-16 11:12:07 +0100

    update copyright/author
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- src/main/java/com/powsybl/openloadflow/lf/outerloop/config/AcOuterLoopConfig.java
+++ src/main/java/com/powsybl/openloadflow/lf/outerloop/config/AcOuterLoopConfig.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
+ * @author Valentin Mouradian {@literal <valentin.mouradian at artelys.com>}
  */
 public interface AcOuterLoopConfig extends OuterLoopConfig<AcOuterLoop> {
 
