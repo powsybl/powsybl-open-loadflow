a1b7dd641addf6f0dad5e0b65c9846aa358d4a5f
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2025-02-05 22:28:53 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/vector/AcVectorizedEquationSystemCreator.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/vector/AcVectorizedEquationSystemCreator.java
@@ -1,9 +1,18 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openloadflow.ac.equations.vector;
 
 import com.powsybl.openloadflow.ac.equations.*;
 import com.powsybl.openloadflow.equations.*;
 import com.powsybl.openloadflow.network.*;
 
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
 public class AcVectorizedEquationSystemCreator extends AcEquationSystemCreator {
 
     protected AcNetworkVector networkVector;
