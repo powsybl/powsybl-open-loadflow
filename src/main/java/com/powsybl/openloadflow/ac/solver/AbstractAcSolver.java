356510bcf85e7b98b8a463f7dcb06b3aae86b5aa
Hadrien <hadrien.godard@artelys.com>
2025-05-20 17:46:32 +0200

    FastDecoupled algorithm: Begin
    Signed-off-by: Hadrien <hadrien.godard@artelys.com>

--- src/main/java/com/powsybl/openloadflow/ac/solver/AbstractAcSolver.java
+++ src/main/java/com/powsybl/openloadflow/ac/solver/AbstractAcSolver.java
@@ -7,15 +7,22 @@
  */
 package com.powsybl.openloadflow.ac.solver;
 
+import com.powsybl.commons.report.ReportNode;
 import com.powsybl.openloadflow.ac.equations.AcEquationType;
 import com.powsybl.openloadflow.ac.equations.AcVariableType;
-import com.powsybl.openloadflow.equations.EquationSystem;
-import com.powsybl.openloadflow.equations.EquationVector;
-import com.powsybl.openloadflow.equations.JacobianMatrix;
-import com.powsybl.openloadflow.equations.TargetVector;
+import com.powsybl.openloadflow.equations.*;
+import com.powsybl.openloadflow.network.LfBus;
+import com.powsybl.openloadflow.network.LfElement;
 import com.powsybl.openloadflow.network.LfNetwork;
+import com.powsybl.openloadflow.util.PerUnit;
+import com.powsybl.openloadflow.util.Reports;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
 
-import java.util.Objects;
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -34,6 +41,8 @@ public abstract class AbstractAcSolver implements AcSolver {
 
     protected boolean detailedReport;
 
+    public static final List<AcEquationType> REPORTED_AC_EQUATION_TYPES = List.of(AcEquationType.BUS_TARGET_P, AcEquationType.BUS_TARGET_Q, AcEquationType.BUS_TARGET_V);
+
     protected AbstractAcSolver(LfNetwork network,
                                EquationSystem<AcVariableType, AcEquationType> equationSystem,
                                JacobianMatrix<AcVariableType, AcEquationType> j,
@@ -46,5 +55,63 @@ public abstract class AbstractAcSolver implements AcSolver {
         this.targetVector = Objects.requireNonNull(targetVector);
         this.equationVector = Objects.requireNonNull(equationVector);
         this.detailedReport = detailedReport;
+    }
+
+    public static List<Pair<Equation<AcVariableType, AcEquationType>, Double>> findLargestMismatches(EquationSystem<AcVariableType, AcEquationType> equationSystem, double[] mismatch, int count) {
+        return equationSystem.getIndex().getSortedEquationsToSolve().stream()
+                .map(equation -> Pair.of(equation, mismatch[equation.getColumn()]))
+                .filter(e -> Math.abs(e.getValue()) > Math.pow(10, -7))
+                .sorted(Comparator.comparingDouble((Map.Entry<Equation<AcVariableType, AcEquationType>, Double> e) -> Math.abs(e.getValue())).reversed())
+                .limit(count)
+                .toList();
+    }
+
+    public static Map<AcEquationType, Pair<Equation<AcVariableType, AcEquationType>, Double>> getLargestMismatchByAcEquationType(EquationSystem<AcVariableType, AcEquationType> equationSystem, double[] mismatch) {
+        return equationSystem.getIndex().getSortedEquationsToSolve().stream()
+                .map(equation -> Pair.of(equation, mismatch[equation.getColumn()]))
+                .collect(Collectors.toMap(e -> e.getKey().getType(), Function.identity(), BinaryOperator.maxBy(Comparator.comparingDouble(e -> Math.abs(e.getValue())))));
+    }
+
+    protected String getEquationTypeDescription(AcEquationType acEquationType) {
+        return switch (acEquationType) {
+            case BUS_TARGET_P -> "P";
+            case BUS_TARGET_Q -> "Q";
+            case BUS_TARGET_V -> "V";
+            default -> null; // not implemented for other ac equation types
+        };
+    }
+
+    public void reportAndLogLargestMismatchByAcEquationType(ReportNode reportNode, EquationSystem<AcVariableType, AcEquationType> equationSystem, double[] mismatch, Logger logger) {
+        Map<AcEquationType, Pair<Equation<AcVariableType, AcEquationType>, Double>> mismatchEquations = getLargestMismatchByAcEquationType(equationSystem, mismatch);
+
+        // report largest mismatches in (P, Q, V) equations
+        for (AcEquationType acEquationType : REPORTED_AC_EQUATION_TYPES) {
+            Optional.ofNullable(mismatchEquations.get(acEquationType))
+                    .ifPresent(equationPair -> {
+                        Equation<AcVariableType, AcEquationType> equation = equationPair.getKey();
+                        double equationMismatch = equationPair.getValue();
+                        int elementNum = equation.getElementNum();
+                        String elementId = equation.getElement(network).map(LfElement::getId).orElse("?");
+                        int busVRow = equationSystem.getVariable(elementNum, AcVariableType.BUS_V).getRow();
+                        int busPhiRow = equationSystem.getVariable(elementNum, AcVariableType.BUS_PHI).getRow();
+                        double busV = equationSystem.getStateVector().get(busVRow);
+                        double busPhi = equationSystem.getStateVector().get(busPhiRow);
+                        LfBus bus = network.getBus(elementNum);
+                        double busNominalV = bus.getNominalV();
+                        double busSumP = bus.getP().eval() * PerUnit.SB;
+                        double busSumQ = bus.getQ().eval() * PerUnit.SB;
+
+                        if (logger.isTraceEnabled()) {
+                            logger.trace("Largest mismatch on {}: {}", getEquationTypeDescription(acEquationType), equationMismatch);
+                            logger.trace("    Bus Id: {} (nominalVoltage={})", elementId, busNominalV);
+                            logger.trace("    Bus  V: {} pu, {} rad", busV, busPhi);
+                        }
+
+                        if (reportNode != null) {
+                            Reports.BusReport busReport = new Reports.BusReport(elementId, equationMismatch, busNominalV, busV, busPhi, busSumP, busSumQ);
+                            Reports.reportNewtonRaphsonLargestMismatches(reportNode, getEquationTypeDescription(acEquationType), busReport);
+                        }
+                    });
+        }
     }
 }
--- src/main/java/com/powsybl/openloadflow/ac/solver/FastDecoupled.java
+++ src/main/java/com/powsybl/openloadflow/ac/solver/FastDecoupled.java
@@ -8,23 +8,37 @@
 package com.powsybl.openloadflow.ac.solver;
 
 import com.powsybl.commons.report.ReportNode;
+import com.powsybl.math.matrix.MatrixException;
 import com.powsybl.openloadflow.ac.equations.AcEquationType;
 import com.powsybl.openloadflow.ac.equations.AcVariableType;
-import com.powsybl.openloadflow.equations.EquationSystem;
-import com.powsybl.openloadflow.equations.EquationVector;
-import com.powsybl.openloadflow.equations.JacobianMatrix;
-import com.powsybl.openloadflow.equations.TargetVector;
+import com.powsybl.openloadflow.equations.*;
+import com.powsybl.openloadflow.network.LfBus;
 import com.powsybl.openloadflow.network.LfNetwork;
 import com.powsybl.openloadflow.network.util.VoltageInitializer;
+import com.powsybl.openloadflow.util.Reports;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Hadrien Godard {@literal <hadrien.godard at artelys.com>}
  */
 public class FastDecoupled extends AbstractAcSolver {
-    public FastDecoupled(LfNetwork network, EquationSystem<AcVariableType, AcEquationType> equationSystem,
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FastDecoupled.class);
+
+    public static final List<AcEquationType> REPORTED_AC_EQUATION_TYPES = List.of(AcEquationType.BUS_TARGET_P, AcEquationType.BUS_TARGET_Q, AcEquationType.BUS_TARGET_V);
+
+    protected final NewtonRaphsonParameters parameters;
+
+    public FastDecoupled(LfNetwork network, NewtonRaphsonParameters parameters, EquationSystem<AcVariableType, AcEquationType> equationSystem,
                          JacobianMatrix<AcVariableType, AcEquationType> j, TargetVector<AcVariableType, AcEquationType> targetVector,
                          EquationVector<AcVariableType, AcEquationType> equationVector, boolean detailedReport) {
         super(network, equationSystem, j, targetVector, equationVector, detailedReport);
+        this.parameters = Objects.requireNonNull(parameters);
     }
 
     @Override
@@ -32,11 +46,79 @@ public class FastDecoupled extends AbstractAcSolver {
         return "Fast Decoupled";
     }
 
+    private AcSolverStatus runIteration(StateVectorScaling svScaling, MutableInt iterations, ReportNode reportNode) {
+        LOGGER.debug("Start iteration {}", iterations);
+    try {
+        // create iteration report
+        // - add 1 to iteration so that it starts at 1 instead of 0
+        ReportNode iterationReportNode = detailedReport ? Reports.createNewtonRaphsonMismatchReporter(reportNode, iterations.getValue() + 1) : null;
+
+        // Phi state vector update
+        // TODO HG: Call method to activate phi equations and variables and deactivate v equations and variables
+        // solve f(x) = j * dx
+        try {
+            j.solveTransposed(equationVector.getArray());
+        } catch (MatrixException e) {
+            LOGGER.error(e.toString(), e);
+            Reports.reportNewtonRaphsonError(reportNode, e.toString());
+            return AcSolverStatus.SOLVER_FAILED;
+        }
+        // f(x) now contains dx
+
+        svScaling.apply(equationVector.getArray(), equationSystem, iterationReportNode);
+
+        // update x and f(x) will be automatically updated
+        equationSystem.getStateVector().minus(equationVector.getArray());
+
+        // V state vector update
+        // TODO HG: Call method to activate v equations and variables and deactivate phi equations and variables
+
+        return null;
+    } finally {
+        iterations.increment();
+    }
+    }
+
     @Override
     public AcSolverResult run(VoltageInitializer voltageInitializer, ReportNode reportNode) {
-//        equationSystem.getEquations().stream().filter()
-//        JacobianMatrix<AcVariableType, AcEquationType> jacobianMatrix1 = new JacobianMatrix<>()
-        return null;
+        // initialize state vector
+        AcSolverUtil.initStateVector(network, equationSystem, voltageInitializer);
 
+        Vectors.minus(equationVector.getArray(), targetVector.getArray());
+
+        NewtonRaphsonStoppingCriteria.TestResult initialTestResult = parameters.getStoppingCriteria().test(equationVector.getArray(), equationSystem);
+        StateVectorScaling svScaling = StateVectorScaling.fromMode(parameters, initialTestResult);
+
+        LOGGER.debug("|f(x0)|={}", initialTestResult.getNorm());
+
+        ReportNode initialReportNode = detailedReport ? Reports.createNewtonRaphsonMismatchReporter(reportNode, 0) : null;
+        if (detailedReport) {
+            Reports.reportNewtonRaphsonNorm(initialReportNode, initialTestResult.getNorm());
+        }
+        if (detailedReport || LOGGER.isTraceEnabled()) {
+            reportAndLogLargestMismatchByAcEquationType(initialReportNode, equationSystem, equationVector.getArray(), LOGGER);
+        }
+
+        // start iterations
+        AcSolverStatus status = AcSolverStatus.NO_CALCULATION;
+        MutableInt iterations = new MutableInt();
+        while (iterations.getValue() <= parameters.getMaxIterations()) {
+            AcSolverStatus newStatus = runIteration(svScaling, iterations, reportNode);
+            if (newStatus != null) {
+                status = newStatus;
+                break;
+            }
+        }
+
+        if (iterations.getValue() >= parameters.getMaxIterations()) {
+            status = AcSolverStatus.MAX_ITERATION_REACHED;
+        }
+
+        if (status == AcSolverStatus.CONVERGED || parameters.isAlwaysUpdateNetwork()) {
+            AcSolverUtil.updateNetwork(network, equationSystem);
+        }
+
+        double slackBusActivePowerMismatch = network.getSlackBuses().stream().mapToDouble(LfBus::getMismatchP).sum();
+        return new AcSolverResult(status, iterations.getValue(), slackBusActivePowerMismatch);
     }
 }
