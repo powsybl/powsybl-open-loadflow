ad247adbf3395c3eb283de58e759e818bb479cdd
Florian Dupuy <florian.dupuy@rte-france.com>
2025-05-22 16:03:26 +0200

    equationDerivativeVectorStartIndices int[] instead of record array
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/equations/EquationDerivativeVectorIndices.java
+++
@@ -1,7 +0,0 @@
-package com.powsybl.openloadflow.equations;
-
-/**
- * @author Florian Dupuy {@literal <florian.dupuy at rte-france.com>}
- */
-public record EquationDerivativeVectorIndices(int iStart, int iEnd) {
-}
