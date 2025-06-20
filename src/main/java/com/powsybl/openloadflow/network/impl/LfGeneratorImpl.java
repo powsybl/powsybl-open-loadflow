8c1933d570d7c3bbcdf95ce542e2e5b94a71f751
PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>
2025-06-20 10:16:21 +0200

    Typo
    Signed-off-by: PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/network/impl/LfGeneratorImpl.java
+++ src/main/java/com/powsybl/openloadflow/network/impl/LfGeneratorImpl.java
@@ -41,7 +41,7 @@ public final class LfGeneratorImpl extends AbstractLfGenerator {
 
     private Double qPercent;
 
-    private boolean isTargetQForcedinReactiveLimits = false;
+    private boolean isTargetQForcedInReactiveLimits = false;
 
     private final boolean forceVoltageControl;
 
@@ -180,11 +180,11 @@ public final class LfGeneratorImpl extends AbstractLfGenerator {
             } else if (computedTargetQ > maxQ) {
                 computedTargetQ = maxQ;
             }
-            if (!isTargetQForcedinReactiveLimits) { // Logging and reporting only on first update to avoid too much messages
+            if (!isTargetQForcedInReactiveLimits) { // Logging and reporting only on first update to avoid too much messages
                 if (targetQ != computedTargetQ) {
                     String message = Reports.reportGeneratorWithUpdatedTargetQ(network.getReportNode(), this, targetQ * PerUnit.SB, maxQ * PerUnit.SB);
                     LOGGER.info(message);
-                    isTargetQForcedinReactiveLimits = true;
+                    isTargetQForcedInReactiveLimits = true;
                 }
             }
             targetQ = computedTargetQ;
