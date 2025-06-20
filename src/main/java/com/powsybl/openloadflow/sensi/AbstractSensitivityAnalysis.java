51204c9646b38030cc9953efea629ddc0afc4a65
jeandemanged <damien.jeandemange@artelys.com>
2025-06-20 13:23:24 +0200

    Fix slack distribution prop remaining margin up/down (#1234)
    Signed-off-by: Damien Jeandemange <damien.jeandemange@artelys.com>
    
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>
    
    Co-authored-by: vmouradian <valentin.mouradian@artelys.com>

--- src/main/java/com/powsybl/openloadflow/sensi/AbstractSensitivityAnalysis.java
+++ src/main/java/com/powsybl/openloadflow/sensi/AbstractSensitivityAnalysis.java
@@ -626,7 +626,8 @@ abstract class AbstractSensitivityAnalysis<V extends Enum<V> & Quantity, E exten
 
     protected List<ParticipatingElement> getParticipatingElements(Collection<LfBus> buses, LoadFlowParameters.BalanceType balanceType, OpenLoadFlowParameters openLoadFlowParameters) {
         ActivePowerDistribution.Step step = ActivePowerDistribution.getStep(balanceType, openLoadFlowParameters.isLoadPowerFactorConstant(), openLoadFlowParameters.isUseActiveLimits());
-        List<ParticipatingElement> participatingElements = step.getParticipatingElements(buses, OptionalDouble.empty()); // The value of the mismatch cannot be known here. It is not needed for the distribution types supported in sensitivity analysis and checked in #checkLoadFlowParameters
+        var participatingBuses = ActivePowerDistribution.filterParticipatingBuses(buses);
+        List<ParticipatingElement> participatingElements = step.getParticipatingElements(participatingBuses, Double.NaN); // The value of the mismatch cannot be known here. It is not needed for the distribution types supported in sensitivity analysis and checked in #checkLoadFlowParameters
         ParticipatingElement.normalizeParticipationFactors(participatingElements);
         return participatingElements;
     }
