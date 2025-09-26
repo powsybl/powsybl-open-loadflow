df02a6732ee5c807f849c4b022d05f56c9e9bbaf
vidaldid-rte <didier.vidal_externe@rte-france.com>
2025-09-26 11:50:14 +0200

    For N-1, Security Analysis or Sensitivity Analysis, start simulations with AC emulation HVDC frozen to previous active set point if new parameter startWithFrozenACEmulation is set to true (#1199)
    Signed-off-by: Didier Vidal <didier.vidal_externe@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/util/Reports.java
+++ src/main/java/com/powsybl/openloadflow/util/Reports.java
@@ -900,6 +900,26 @@ public final class Reports {
                 .add();
     }
 
+    public static void reportFreezeHvdc(ReportNode reportNode, String hvdcId, String stationId, double setPoint, Logger logger) {
+        ReportNode node = reportNode.newReportNode()
+                .withMessageTemplate("olf.freezeHvdc")
+                .withUntypedValue("hvdcId", hvdcId)
+                .withUntypedValue("stationId", stationId)
+                .withUntypedValue("setPoint", setPoint)
+                .withSeverity(TypedValue.INFO_SEVERITY)
+                .add();
+        logger.info(node.getMessage());
+    }
+
+    public static void reportUnfreezeHvdc(ReportNode reportNode, String hvdcID, Logger logger) {
+        ReportNode node = reportNode.newReportNode()
+                .withMessageTemplate("olf.unfreezeHvdc")
+                .withUntypedValue("ID", hvdcID)
+                .withSeverity(TypedValue.INFO_SEVERITY)
+                .add();
+        logger.info(node.getMessage());
+    }
+
     public static void reportActionApplicationFailure(String actionId, String contingencyId, ReportNode node) {
         node.newReportNode()
                 .withMessageTemplate("olf.LfActionUtils")
@@ -915,4 +935,5 @@ public final class Reports {
                 .withMessageTemplate("olf.threadRoot")
                 .build();
     }
+
 }
