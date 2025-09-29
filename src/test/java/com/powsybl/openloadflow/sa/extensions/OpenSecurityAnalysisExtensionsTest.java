4450bdd1b2a05015aa8512057c2ebec46eb7740f
Bertrand Rix <bertrand.rix@artelys.com>
2025-09-29 10:34:03 +0200

    Clean
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- src/test/java/com/powsybl/openloadflow/sa/extensions/OpenSecurityAnalysisExtensionsTest.java
+++ src/test/java/com/powsybl/openloadflow/sa/extensions/OpenSecurityAnalysisExtensionsTest.java
@@ -31,7 +31,6 @@ import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
