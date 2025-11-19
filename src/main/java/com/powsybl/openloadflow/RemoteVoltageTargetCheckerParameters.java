d985ad9a24d97b8aace12864afdd41d6c3311260
Didier Vidal <didier.vidal_externe@rte-france.com>
2025-11-19 17:42:16 +0100

    remove unused parameter
    Signed-off-by: Didier Vidal <didier.vidal_externe@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/RemoteVoltageTargetCheckerParameters.java
+++ src/main/java/com/powsybl/openloadflow/RemoteVoltageTargetCheckerParameters.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2024, RTE (http://www.rte-france.com)
+/*
+ * Copyright (c) 2024-2025, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -20,15 +20,11 @@ public class RemoteVoltageTargetCheckerParameters {
 
     public static final double TARGET_VOLTAGE_PLAUSIBILITY_THRESHOLD_DEFAULT_VALUE = 10;
 
-    public static final double CONTROLLER_BUS_ACCEPTABLE_VOLTAGE_SHIFT_DEFAULT_VALUE = 0.8;
-
     private MatrixFactory matrixFactory;
 
     private int controlledBusNeighborsExplorationDepth = CONTROLLED_BUS_NEIGHBORS_EXPLORATION_DEPTH_DEFAULT_VALUE;
 
     private double targetVoltagePlausibilityIndicatorThreshold = TARGET_VOLTAGE_PLAUSIBILITY_THRESHOLD_DEFAULT_VALUE;
-
-    private double controllerBusAcceptableVoltageDrop = CONTROLLER_BUS_ACCEPTABLE_VOLTAGE_SHIFT_DEFAULT_VALUE; // shift from 1 pu
 
     public RemoteVoltageTargetCheckerParameters(MatrixFactory matrixFactory) {
         this.matrixFactory = Objects.requireNonNull(matrixFactory);
@@ -60,20 +56,10 @@ public class RemoteVoltageTargetCheckerParameters {
         return this;
     }
 
-    public double getControllerBusAcceptableVoltageDrop() {
-        return controllerBusAcceptableVoltageDrop;
-    }
-
-    public RemoteVoltageTargetCheckerParameters setControllerBusAcceptableVoltageDrop(double controllerBusAcceptableVoltageDrop) {
-        this.controllerBusAcceptableVoltageDrop = controllerBusAcceptableVoltageDrop;
-        return this;
-    }
-
     public String toString() {
         return "TargetVoltageCompatibilityCheckerParameters(" +
                 "controlledBusNeighborsExplorationDepth=" + controlledBusNeighborsExplorationDepth +
                 ", targetVoltagePlausibilityIndicatorThreshold=" + targetVoltagePlausibilityIndicatorThreshold +
-                ", controllerBusAcceptableVoltageDrop=" + controllerBusAcceptableVoltageDrop +
                 ", matrixFactory=" + matrixFactory.getClass().getSimpleName() +
                 ")";
     }
