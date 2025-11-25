2646134df6515c354e1a54dd446f4ef99d6e279f
Damien Jeandemange <damien.jeandemange@artelys.com>
2025-11-25 15:29:38 +0100

    Maximum number of Shunt sections change per Shunt incremental outerloop (#1297)
    Signed-off-by: Damien Jeandemange <damien.jeandemange@artelys.com>

--- src/test/java/com/powsybl/openloadflow/OpenLoadFlowProviderTest.java
+++ src/test/java/com/powsybl/openloadflow/OpenLoadFlowProviderTest.java
@@ -85,7 +85,7 @@ class OpenLoadFlowProviderTest {
     @Test
     void specificParametersTest() {
         OpenLoadFlowProvider provider = new OpenLoadFlowProvider();
-        assertEquals(78, provider.getSpecificParameters().size());
+        assertEquals(79, provider.getSpecificParameters().size());
         LoadFlowParameters parameters = new LoadFlowParameters();
 
         provider.loadSpecificParameters(Collections.emptyMap())
@@ -110,7 +110,7 @@ class OpenLoadFlowProviderTest {
         Map<String, String> map = provider.createMapFromSpecificParameters(parametersExt);
         // Null values are not serialized by the provider
         long nullValueCOunt = parametersExt.toMap().values().stream().filter(Objects::isNull).count();
-        assertEquals(78, map.size() + nullValueCOunt);
+        assertEquals(79, map.size() + nullValueCOunt);
         assertEquals(2, nullValueCOunt); // debugDir and outerLoopNames are nullable
         assertEquals(provider.getSpecificParameters().size(), map.size() + nullValueCOunt);
     }
