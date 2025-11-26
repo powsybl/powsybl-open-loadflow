7f53c86af5fdb7dba483a88b2e8767fb825227ad
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2025-11-26 14:49:02 +0100

    Fix copyright
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/vector/ClosedBranchEquationTermArrayEvaluator.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/vector/ClosedBranchEquationTermArrayEvaluator.java
@@ -1,8 +1,17 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openloadflow.ac.equations.vector;
 
 import com.powsybl.openloadflow.ac.equations.AcVariableType;
 import com.powsybl.openloadflow.equations.Variable;
 
+/**
+ * @author Sylvestre Prabakaran {@literal <sylvestre.prabakaran at rte-france.com>}
+ */
 public interface ClosedBranchEquationTermArrayEvaluator {
 
     double y(int branchNum);
