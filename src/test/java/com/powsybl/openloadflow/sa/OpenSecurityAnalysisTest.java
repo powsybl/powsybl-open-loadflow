69c7fafed4d39135771df8b8dc6901f84433a30e
PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>
2025-11-28 16:23:49 +0100

    Checkstyle
    Signed-off-by: PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
+++ src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
@@ -4249,8 +4249,6 @@ class OpenSecurityAnalysisTest extends AbstractOpenSecurityAnalysisTest {
         assertEquals(LoadFlowResult.ComponentResult.Status.CONVERGED, saResultMainSynchronous.getPreContingencyResult().getStatus());
         assertEquals(2, saResultMainSynchronous.getPreContingencyResult().getNetworkResult().getBusResults().size()); // 2 buses in SC0 (that is in CC1)
         assertEquals(1.0, saResultMainSynchronous.getPreContingencyResult().getNetworkResult().getBusResult("b11").getV()); // There is a result in a bus of CC1
-
-
     }
 
     @ParameterizedTest
