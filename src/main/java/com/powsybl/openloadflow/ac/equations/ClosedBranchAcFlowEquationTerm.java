b1fbe3739b427b6b7df912eba9ad85f9dc041ebd
PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>
2025-12-01 17:32:58 +0100

    Remove useless implement
    Signed-off-by: PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/equations/ClosedBranchAcFlowEquationTerm.java
+++ src/main/java/com/powsybl/openloadflow/ac/equations/ClosedBranchAcFlowEquationTerm.java
@@ -7,13 +7,12 @@
  */
 package com.powsybl.openloadflow.ac.equations;
 
-import com.powsybl.openloadflow.equations.EquationTerm;
 import com.powsybl.openloadflow.equations.Variable;
 
 /**
  * @author Sylvestre Prabakaran {@literal <sylvestre.prabakaran at rte-france.com>}
  */
-public interface ClosedBranchAcFlowEquationTerm extends EquationTerm<AcVariableType, AcEquationType> {
+public interface ClosedBranchAcFlowEquationTerm {
 
     double y();
 
