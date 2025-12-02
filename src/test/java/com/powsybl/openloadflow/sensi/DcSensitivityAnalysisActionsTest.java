07e6777c85647f23367c068c510a4061068cd8a4
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2025-12-02 09:25:41 +0100

    Add sensi action unit test
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ src/test/java/com/powsybl/openloadflow/sensi/DcSensitivityAnalysisActionsTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.openloadflow.sensi;
+
+import com.powsybl.action.Action;
+import com.powsybl.action.TerminalsConnectionAction;
+import com.powsybl.contingency.BranchContingency;
+import com.powsybl.contingency.Contingency;
+import com.powsybl.contingency.ContingencyContext;
+import com.powsybl.contingency.strategy.ConditionalActions;
+import com.powsybl.contingency.strategy.OperatorStrategy;
+import com.powsybl.contingency.strategy.condition.TrueCondition;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.openloadflow.network.FourBusNetworkFactory;
+import com.powsybl.sensitivity.SensitivityAnalysisParameters;
+import com.powsybl.sensitivity.SensitivityAnalysisResult;
+import com.powsybl.sensitivity.SensitivityAnalysisRunParameters;
+import com.powsybl.sensitivity.SensitivityFactor;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
+ */
+class DcSensitivityAnalysisActionsTest extends AbstractSensitivityAnalysisTest {
+
+    @Test
+    void test() {
+        Network network = FourBusNetworkFactory.create();
+        runDcLf(network);
+
+        SensitivityAnalysisParameters sensiParameters = createParameters(true, "b1_vl_0", true);
+        sensiParameters.getLoadFlowParameters().setBalanceType(LoadFlowParameters.BalanceType.PROPORTIONAL_TO_GENERATION_P_MAX);
+
+        List<Contingency> contingencies = List.of(new Contingency("l23", new BranchContingency("l23")));
+
+        List<SensitivityFactor> factors = createFactorMatrix(List.of(network.getGenerator("g2")),
+                network.getBranchStream().toList());
+
+        List<OperatorStrategy> operatorStrategies = List.of(new OperatorStrategy("reclose l23",
+                                                                                 ContingencyContext.all(),
+                                                                                 List.of(new ConditionalActions("always reclose l23", new TrueCondition(), List.of("reclose l23")))));
+        List<Action> actions = List.of(new TerminalsConnectionAction("reclose l23", "l23", false));
+        SensitivityAnalysisResult result = sensiRunner.run(network, factors, new SensitivityAnalysisRunParameters()
+                .setContingencies(contingencies)
+                .setParameters(sensiParameters)
+                .setOperatorStrategies(operatorStrategies)
+                .setActions(actions));
+
+        for (var value : result.getValues()) {
+            System.out.println(value);
+        }
+    }
+}
