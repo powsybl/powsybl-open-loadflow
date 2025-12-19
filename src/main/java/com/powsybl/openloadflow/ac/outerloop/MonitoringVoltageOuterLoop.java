7fea4d08afc498339eab498b523bc13972cd4518
Didier Vidal <didier.vidal_externe@rte-france.com>
2025-12-19 17:40:24 +0100

    Remove VoltageMonitoring outerloop when not needed by network
    Signed-off-by: Didier Vidal <didier.vidal_externe@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/outerloop/MonitoringVoltageOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/ac/outerloop/MonitoringVoltageOuterLoop.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+/*
+ * Copyright (c) 2022-2025, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -8,6 +8,7 @@
 package com.powsybl.openloadflow.ac.outerloop;
 
 import com.powsybl.commons.report.ReportNode;
+import com.powsybl.openloadflow.ac.AcLoadFlowContext;
 import com.powsybl.openloadflow.ac.AcOuterLoopContext;
 import com.powsybl.openloadflow.lf.outerloop.OuterLoopResult;
 import com.powsybl.openloadflow.lf.outerloop.OuterLoopStatus;
@@ -139,5 +140,12 @@ public class MonitoringVoltageOuterLoop implements AcOuterLoop {
         }
 
         return new OuterLoopResult(this, status);
+    }
+
+    @Override
+    public boolean isNeeded(AcLoadFlowContext context) {
+        return context.getNetwork().<LfBus>getControllerElements(VoltageControl.Type.GENERATOR).stream()
+                .filter(bus -> bus.isGeneratorVoltageControlled())
+                .anyMatch(bus -> getControlledBusVoltageLimits(bus).isPresent());
     }
 }
