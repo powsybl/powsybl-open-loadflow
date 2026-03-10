4d9ed37ab8523febe1d42084df0fcf91f05d482f
b.perreyon <baptiste.perreyon@supergrid-institute.com>
2026-03-10 10:51:37 +0100

    Add licence and authorship of VoltageSourceConverterVoltageControl.java
    Signed-off-by: b.perreyon <baptiste.perreyon@supergrid-institute.com>

--- src/main/java/com/powsybl/openloadflow/network/VoltageSourceConverterVoltageControl.java
+++ src/main/java/com/powsybl/openloadflow/network/VoltageSourceConverterVoltageControl.java
@@ -1,5 +1,15 @@
-package com.powsybl.openloadflow.network;
+/**
+ * Copyright (c) 2025, SuperGrid Institute (http://www.supergrid-institute.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 
+package com.powsybl.openloadflow.network;
+/**
+ * @author Baptiste Perreyon {@literal <baptiste.perreyon at supergrid-institute.com>}
+ */
 public class VoltageSourceConverterVoltageControl extends VoltageControl<LfBus> {
 
     public VoltageSourceConverterVoltageControl(LfBus controlledBus, int targetPriority, double targetValue) {
