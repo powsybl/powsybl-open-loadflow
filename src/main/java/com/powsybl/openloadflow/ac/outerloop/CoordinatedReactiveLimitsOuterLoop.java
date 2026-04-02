a0ac43cabf396ccdef6ef89dc3d7308c8ed59b7c
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2026-04-02 10:36:22 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/ac/outerloop/CoordinatedReactiveLimitsOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/ac/outerloop/CoordinatedReactiveLimitsOuterLoop.java
@@ -39,6 +39,7 @@ public class CoordinatedReactiveLimitsOuterLoop implements AcOuterLoop {
     private static final double Q_LIMIT_EPSILON = 1E-6; // 10-3 MVar in PU
     private static final double SENSI_EPS = 1e-6;
     private static final double DV_EPS = 1e-8;
+    private static final double MAX_DV_KV = 1.0;
 
     private final MatrixFactory matrixFactory;
 
@@ -61,100 +62,104 @@ public class CoordinatedReactiveLimitsOuterLoop implements AcOuterLoop {
 
         // check if some controller buses are out of their limits
         List<String> controllerBusIdsToAdjust = new ArrayList<>();
-        for (LfBus controllerBus : context.getNetwork().<LfBus>getControllerElements(VoltageControl.Type.GENERATOR)) {
-            double minQ = controllerBus.getMinQ();
-            double maxQ = controllerBus.getMaxQ();
-            double q = controllerBus.getQ().eval() + controllerBus.getLoadTargetQ();
-            if (q < minQ - Q_LIMIT_EPSILON) {
-                LOGGER.trace("Need to adjust controller bus '{}' from {} to min limit {}",
-                        controllerBus.getId(), q * PerUnit.SB, minQ * PerUnit.SB);
-                controllerBusIdsToAdjust.add(controllerBus.getId());
-            } else if (q > maxQ + Q_LIMIT_EPSILON) {
-                LOGGER.trace("Need to adjust controller bus '{}' from {} to max limit {}",
-                        controllerBus.getId(), q * PerUnit.SB, maxQ * PerUnit.SB);
-                controllerBusIdsToAdjust.add(controllerBus.getId());
+        List<LfBus> controlledBuses = context.getNetwork().<LfBus>getControlledBuses(VoltageControl.Type.GENERATOR)
+                .stream()
+                .filter(LfBus::isGeneratorVoltageControlEnabled)
+                .toList();
+        for (LfBus controlledBus : controlledBuses) {
+            for (LfBus controllerBus : controlledBus.getGeneratorVoltageControl().orElseThrow().getControllerElements()) {
+                double minQ = controllerBus.getMinQ();
+                double maxQ = controllerBus.getMaxQ();
+                double q = controllerBus.getQ().eval() + controllerBus.getLoadTargetQ();
+                if (q < minQ) {
+                    LOGGER.trace("Need to adjust controller bus '{}' from {} to min limit {}",
+                            controllerBus.getId(), q * PerUnit.SB, minQ * PerUnit.SB);
+                    controllerBusIdsToAdjust.add(controllerBus.getId());
+                } else if (q > maxQ) {
+                    LOGGER.trace("Need to adjust controller bus '{}' from {} to max limit {}",
+                            controllerBus.getId(), q * PerUnit.SB, maxQ * PerUnit.SB);
+                    controllerBusIdsToAdjust.add(controllerBus.getId());
+                }
             }
         }
         LOGGER.debug("{} controller buses need to be adjusted", controllerBusIdsToAdjust.size());
 
         if (!controllerBusIdsToAdjust.isEmpty()) {
-            List<LfBus> controlledBuses = context.getNetwork().<LfBus>getControlledBuses(VoltageControl.Type.GENERATOR)
-                    .stream()
-                    .filter(LfBus::isGeneratorVoltageControlEnabled)
+            List<LfBus> firstControllerBuses = controlledBuses.stream()
+                    .map(b -> {
+                        var controllerBuses = b.getGeneratorVoltageControl().orElseThrow().getControllerElements();
+                        if (controllerBuses.size() > 1) {
+                            throw new PowsyblException("Shared remote voltage control not supported: "
+                                    + controllerBuses.stream().map(LfBus::getId).toList());
+                        }
+                        return controllerBuses.getFirst();
+                    })
+                    .distinct()
                     .toList();
 
-            if (!controlledBuses.isEmpty()) {
-                List<LfBus> firstControllerBuses = controlledBuses.stream()
-                        .map(b -> {
-                            var controllerBuses = b.getGeneratorVoltageControl().orElseThrow().getControllerElements();
-                            if (controllerBuses.size() > 1) {
-                                throw new PowsyblException("Shared remote voltage control not supported: "
-                                        + controllerBuses.stream().map(LfBus::getId).toList());
-                            }
-                            return controllerBuses.getFirst();
-                        })
-                        .distinct()
-                        .toList();
+            var controlledBusIndex = LfBus.buildIndex(controlledBuses);
+            var controllerBusIndex = LfBus.buildIndex(firstControllerBuses);
+            SensitivityContext sensitivityContext = SensitivityContext.create(controlledBuses,
+                    controlledBusIndex,
+                    context.getLoadFlowContext());
 
-                var controlledBusIndex = LfBus.buildIndex(controlledBuses);
-                var controllerBusIndex = LfBus.buildIndex(firstControllerBuses);
-                SensitivityContext sensitivityContext = SensitivityContext.create(controlledBuses,
-                        controlledBusIndex,
-                        context.getLoadFlowContext());
-
-                // q + dqdv1 * dv1 + dqdv2 * dv2 + ... + dqdvn * dvn = qLimitMax - epsilon
-                // or
-                // q + dqdv1 * dv1 / dqdv2 * dv2 + ... + dqdvn * dvn = qLimitMin + epsilon
-                Matrix j = matrixFactory.create(firstControllerBuses.size(), controlledBuses.size(), firstControllerBuses.size());
-                for (LfBus controlledBus : controlledBuses) {
-                    for (LfBus firstControllerBus : firstControllerBuses) {
-                        double s = sensitivityContext.calculateSensiQ(firstControllerBus, controlledBus);
-                        int row = controllerBusIndex.get(firstControllerBus.getNum());
-                        int col = controlledBusIndex.get(controlledBus.getNum());
-                        if (Math.abs(s) >= SENSI_EPS) {
-                            j.add(row, col, s);
-                        }
-                    }
-                }
-
-                double[] dq = new double[firstControllerBuses.size()];
+            // q + dqdv1 * dv1 + dqdv2 * dv2 + ... + dqdvn * dvn = qLimitMax - epsilon
+            // or
+            // q + dqdv1 * dv1 / dqdv2 * dv2 + ... + dqdvn * dvn = qLimitMin + epsilon
+            Matrix j = matrixFactory.create(firstControllerBuses.size(), controlledBuses.size(), firstControllerBuses.size());
+            for (LfBus controlledBus : controlledBuses) {
                 for (LfBus firstControllerBus : firstControllerBuses) {
-                    double minQ = firstControllerBus.getMinQ();
-                    double maxQ = firstControllerBus.getMaxQ();
-                    double q = firstControllerBus.getQ().eval() + firstControllerBus.getLoadTargetQ();
+                    double s = sensitivityContext.calculateSensiQ(firstControllerBus, controlledBus);
                     int row = controllerBusIndex.get(firstControllerBus.getNum());
-                    if (q < minQ) {
-                        dq[row] = minQ - q;
-                    } else if (q > maxQ) {
-                        dq[row] = maxQ - q;
-                    } else {
-                        dq[row] = 0;
+                    int col = controlledBusIndex.get(controlledBus.getNum());
+                    if (Math.abs(s) >= SENSI_EPS) {
+                        j.add(row, col, s);
                     }
                 }
-
-                try (LUDecomposition luDecomposition = j.decomposeLU()) {
-                    luDecomposition.solve(dq);
-                }
-
-                List<String> controlledBusIdsToAdjust = new ArrayList<>();
-                for (LfBus controlledBus : controlledBuses) {
-                    int row = controlledBusIndex.get(controlledBus.getNum());
-                    double dv = dq[row];
-                    if (Math.abs(dv) > DV_EPS) {
-                        var vc = controlledBus.getGeneratorVoltageControl().orElseThrow();
-                        var newTargetValue = vc.getTargetValue() + dv;
-                        LOGGER.trace("Adjust target voltage of controlled bus '{}': {} -> {}",
-                                controlledBus.getId(), vc.getTargetValue() * controlledBus.getNominalV(),
-                                newTargetValue * controlledBus.getNominalV());
-                        vc.setTargetValue(newTargetValue);
-                        controlledBusIdsToAdjust.add(controlledBus.getId());
-                    }
-                }
-                LOGGER.debug("{} controlled buses have been adjusted", controlledBusIdsToAdjust.size());
             }
+
+            double[] dq = new double[firstControllerBuses.size()];
+            for (LfBus firstControllerBus : firstControllerBuses) {
+                double minQ = firstControllerBus.getMinQ();
+                double maxQ = firstControllerBus.getMaxQ();
+                double q = firstControllerBus.getQ().eval() + firstControllerBus.getLoadTargetQ();
+                int row = controllerBusIndex.get(firstControllerBus.getNum());
+                if (q < minQ) {
+                    dq[row] = minQ - q + Q_LIMIT_EPSILON;
+                } else if (q > maxQ) {
+                    dq[row] = maxQ - q - Q_LIMIT_EPSILON;
+                } else {
+                    dq[row] = 0;
+                }
+            }
+
+            try (LUDecomposition luDecomposition = j.decomposeLU()) {
+                luDecomposition.solve(dq);
+            }
+
+            List<String> controlledBusIdsToAdjust = new ArrayList<>();
+            for (LfBus controlledBus : controlledBuses) {
+                int row = controlledBusIndex.get(controlledBus.getNum());
+                double dv = dq[row];
+                if (Math.abs(dv) > DV_EPS) {
+                    var vc = controlledBus.getGeneratorVoltageControl().orElseThrow();
+                    double dvKv = dv * vc.getTargetValue() * controlledBus.getNominalV();
+                    if (Math.abs(dvKv) > MAX_DV_KV) {
+                        LOGGER.error("Voltage control adjustment for bus '{}' exceeds maximum allowed change: {} Kv", controlledBus.getId(), dvKv);
+                    }
+                    var newTargetValue = vc.getTargetValue() + dv;
+                    LOGGER.trace("Adjust target voltage of controlled bus '{}': {} -> {}",
+                            controlledBus.getId(), vc.getTargetValue() * controlledBus.getNominalV(),
+                            newTargetValue * controlledBus.getNominalV());
+                    vc.setTargetValue(newTargetValue);
+                    controlledBusIdsToAdjust.add(controlledBus.getId());
+                }
+            }
+            LOGGER.debug("{} controlled buses have been adjusted", controlledBusIdsToAdjust.size());
 
             outerLoopStatus = OuterLoopStatus.UNSTABLE;
         }
+
         return new OuterLoopResult(NAME, outerLoopStatus);
     }
 
