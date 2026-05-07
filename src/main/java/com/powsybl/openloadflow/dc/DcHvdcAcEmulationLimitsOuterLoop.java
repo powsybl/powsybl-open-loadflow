feea6bb46c4c22cd1a439f584a6334dcf0b13dde
Sylvestre Prabakaran <sylvestre.prabakaran@rte-france.com>
2026-05-07 16:13:47 +0200

    Checkstyle
    Signed-off-by: Sylvestre Prabakaran <sylvestre.prabakaran@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/dc/DcHvdcAcEmulationLimitsOuterLoop.java
+++ src/main/java/com/powsybl/openloadflow/dc/DcHvdcAcEmulationLimitsOuterLoop.java
@@ -8,7 +8,6 @@
 package com.powsybl.openloadflow.dc;
 
 import com.powsybl.commons.report.ReportNode;
-import com.powsybl.openloadflow.ac.AcLoadFlowContext;
 import com.powsybl.openloadflow.dc.equations.DcVariableType;
 import com.powsybl.openloadflow.dc.equations.DcEquationType;
 import com.powsybl.openloadflow.lf.outerloop.AbstractHvdcAcEmulationLimitsOuterLoop;
