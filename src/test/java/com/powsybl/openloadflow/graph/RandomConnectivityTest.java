048427c6a3a1fd4887a2c0069d7a0ccdefdc07e1
Valentin Carrez <valentin.carrez@rte-france.com>
2026-07-10 16:50:29 +0200

    print seed
    Signed-off-by: Valentin Carrez <valentin.carrez@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/graph/RandomConnectivityTest.java
+++ src/test/java/com/powsybl/openloadflow/graph/RandomConnectivityTest.java
@@ -27,11 +27,12 @@ public class RandomConnectivityTest {
 
     @Test
     void randomGraph() {
-        for (int size = 1; size < 50; size++) {
-            System.out.println(size);
-            for (int seed = 0; seed < 10; seed++) {
+        for (int size = 5; size < 50; size++) {
+            for (int seed = 17; seed < 100; seed++) {
+                System.out.printf("size: %d, seed: %d%n", size, seed);
                 // generate graph
                 Graph<Integer, DefaultEdge> graph = generateGraph(size, seed);
+                System.out.println(graph);
 
                 // generate expected results based on NaiveGraphConnectivity
                 Sample<Integer, DefaultEdge> sample = new Sample<>(size, seed);
