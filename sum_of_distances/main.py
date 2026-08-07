3ec121e004abd8ef8116c8c0274df8b569e5a41f
Valentin Carrez <valentin.carrez@rte-france.com>
2026-08-07 17:10:34 +0200

    better graph with pyqtgraph
    Signed-off-by: Valentin Carrez <valentin.carrez@rte-france.com>

--- sum_of_distances/main.py
+++ sum_of_distances/main.py
@@ -9,8 +9,13 @@ from enum import Enum
 from pathlib import Path
 from typing import Tuple
 
-import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
+import numpy as np
+import pyqtgraph as pg
+import qdarktheme
+from PyQt6 import QtCore
+from PyQt6.QtWidgets import QMainWindow
+from pyqtgraph import mkPen, PlotItem, PlotDataItem, PlotCurveItem
+from pyqtgraph.graphicsItems import ScatterPlotItem, InfiniteLine
 
 
 class Method(Enum):
@@ -30,66 +35,215 @@ class Method(Enum):
     GET_EDGES_REMOVED_FROM_MAIN_COMPONENT = "e_removed"
 
 
-class Operations:
-    file: Path
+@dataclass
+class OperationsResult:
+    operations_file: str
     operations: list[Tuple[Method, int]]
 
     def __init__(self, file: Path):
         self.file = file
         with file.open(mode='r', encoding='utf-8') as f:
-            self.operations = [Operations.parse_line(line) for line in f]
+            line = f.readline()
+
+            if line.startswith("source "):
+                self.operations_file = line.removeprefix("source").strip()
+                self.operations = []
+            else:
+                self.operations_file = file.name
+                self.operations = [OperationsResult.parse_line(line)]
+
+            self.operations += [OperationsResult.parse_line(line) for line in f]
 
     @staticmethod
     def parse_line(line: str) -> Tuple[Method, int]:
         parts = line.split(" ")
         return Method(parts[0]), int(parts[1])
 
+
 @dataclass
-class Data:
-    graph_count: int
-    per_connectivity: dict[str, list[Operations]]
+class ConnectivityOperationsResult:
+    operations_file: str
+    # map a connectivity implementation (and implicitly an operations file) to associated results
+    per_connectivity: dict[str, OperationsResult]
 
-def get_data(path: Path) -> Data:
-    data = {}
-    graph_count = 0
-    for conn in path.iterdir():
-        op = [Operations(operation_res) for operation_res in conn.iterdir()]
-        op.sort(key=lambda op: op.file)
-        graph_count = max(graph_count, len(op))
-        data[conn.name] = op
+    def set(self, connectivity: str, results: OperationsResult):
+        if connectivity in self.per_connectivity:
+            raise ValueError("Duplicate results for ", connectivity)
 
-    return Data(graph_count, data)
+        self.per_connectivity[connectivity] = results
 
-def plot(ax: Axes, data: Data, i: int):
-    for connectivity, op in data.per_connectivity.items():
-        y_data = op[i].operations
-        x_data = range(0, len(y_data))
 
-        ax.plot(x_data, [sd for (_, sd) in y_data], label=connectivity)
-        ax.set_title(op[i].file.name)
+@dataclass
+class Results:
+    # map an Operations (aka either a single threaded workload or sub-workload of a multithreaded workload)
+    # to associated results for all connectivity
+    per_operations: dict[str, ConnectivityOperationsResult]
+
+    def get(self, operations: str) -> ConnectivityOperationsResult:
+        if operations not in self.per_operations:
+            self.per_operations[operations] = ConnectivityOperationsResult(operations, {})
+
+        return self.per_operations[operations]
+
+    def __len__(self):
+        return len(self.per_operations)
+
+
+class ConnectivityOperationsResultPlot(PlotItem):
+
+    def __init__(self, res: ConnectivityOperationsResult, **kwargs):
+        super().__init__(**kwargs)
+        self.setTitle(res.operations_file)
+        self.addLegend()
+        self.tooltips = {}
+
+        for i, (connectivity, op) in enumerate(res.per_connectivity.items()):
+            y_data = op.operations
+            x_data = range(0, len(y_data))
+
+            pen = mkPen(color=(i, len(res.per_connectivity)))
+
+            plot = self.plot()
+            plot.curve = HoverPlotCurveItem(self, connectivity, op)
+            plot.curve.setParentItem(plot)
+            plot.setData(x_data, [sd for (_, sd) in y_data])
+            plot.setPen(pen)
+
+            self.legend.addItem(plot, connectivity)
+
+        self.guideline = pg.InfiniteLine(angle=90, pen='r')
+        self.guideline.setVisible(False)
+        self.addItem(self.guideline)
+
+    def update_tooltip(self, plot: PlotCurveItem, tooltip: str):
+        self.tooltips[plot] = str(tooltip)
+
+        vb = plot.getViewBox()
+        full_tooltip = "\n".join(self.tooltips.values())
+        if vb is not None:
+            vb.setToolTip(full_tooltip)
+
+        self.upper_left_text.setText(full_tooltip)
+
+    def set_guideline(self, x: int):
+        if x >= 0:
+            self.guideline.setPos(x)
+            self.guideline.setVisible(True)
+        else:
+            self.guideline.setVisible(False)
+
+
+class HoverPlotCurveItem(PlotCurveItem):
+
+    def __init__(self, plot_item: ConnectivityOperationsResultPlot, connectivity: str, operations: OperationsResult, *args, **kargs):
+        super().__init__(*args, **kargs)
+        self.plot_item = plot_item
+        self.connectivity = connectivity
+        self.operations = operations
+
+    def nearest_point_index(self, obj) -> int:
+        if isinstance(obj, QtCore.QPointF):
+            l = obj.x() - 0.5 # - 0.5 to account for integer precision and have proper guideline and tooltip around the mouse
+        else:
+            raise TypeError
+
+        i = np.clip(np.searchsorted(self.xData, l), 0, len(self.xData) - 1)
+
+        if abs(self.xData[i] - l) > 5:
+            return -1
+        else:
+            return i
+
+    def hoverEvent(self, ev):
+        vb = self.getViewBox()
+        if vb is None:
+            return
+
+        if ev.exit:
+            self.plot_item.update_tooltip(self, "")
+        else:
+            i = self.nearest_point_index(ev.pos())
+
+            if vb is not None:
+                if 0 <= i < len(self.xData):
+                    self.plot_item.update_tooltip(self, self.tooltip_for(i))
+                    self.plot_item.set_guideline(i)
+                else:
+                    self.plot_item.update_tooltip(self, "")
+                    self.plot_item.set_guideline(-1)
+            else:
+                self.plot_item.set_guideline(-1)
+
+
+    def tooltip_for(self, x_index: int) -> str:
+        method, Sd = self.operations.operations[x_index]
+
+        return "{conn}: {method} ({i}) - Sd: {Sd}".format(
+            conn=self.connectivity,
+            method=method,
+            i=x_index,
+            Sd = Sd
+        )
+
+def get_data(path: Path) -> Results:
+    results = Results({})
+
+    # iterate over results per connectivity
+    for connectivity in path.iterdir():
+        # iterate over results per operations
+        for operation_res in connectivity.iterdir():
+            op_res = OperationsResult(operation_res)
+
+            conn_res = results.get(op_res.operations_file)
+            conn_res.set(connectivity.name, op_res)
+
+    return results
+
+
+class Window(QMainWindow):
+
+    def __init__(self, results: Results):
+        super().__init__()
+        self.results = results
+
+        self.setCentralWidget(self.plot_all())
+        self.show()
+        self.setWindowTitle("Results")
+        self.resize(1200, 1200 * 3 // 4)
+        self.center_window()
+
+    def plot_all(self) -> pg.GraphicsLayoutWidget:
+        w = int(math.ceil(math.sqrt(len(self.results))))
+        h = int(math.ceil(len(self.results) / w))
+        print(w, h)
+
+        graphics = pg.GraphicsLayoutWidget()
+
+        for i, operations in enumerate(self.results.per_operations.values()):
+            x = i % w
+            y = i // w
+
+            plot = ConnectivityOperationsResultPlot(operations)
+            graphics.ci.addItem(plot, col=x, row=y)
+
+        return graphics
+
+    def center_window(self):
+        qr = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
 
 if __name__ == '__main__':
-    workload = Path("data/spy_10000_10_10_10000_10_10_2026-07-09T08:47:18.906235251Z.zip/")
+    workload = Path("data/spy_10000_10_10_10000_10_10_2026-08-07T07:59:16.649371906Z.zip/")
     data = get_data(workload)
 
-    w = int(math.ceil(math.sqrt(data.graph_count)))
-    h = int(math.ceil(data.graph_count / w))
-    print(w, h)
+    pg.setConfigOptions(antialias=True)
+    pg.setConfigOption('background', 'w')
+    pg.setConfigOption('foreground', 'k')
+    pg.mkQApp("Results")
 
-    fig, axs = plt.subplots(w, h)
-
-    for x in range(w):
-        for y in range(h):
-            i = y * w + x
-            if w == 1:
-                ax = axs
-            elif h == 1:
-                ax = axs[x]
-            else:
-                ax = axs[x, y]
-
-            plot(ax, data, i)
-
-    plt.tight_layout(pad=1.01)
-    plt.legend()
-    plt.show()
+    window = Window(data)
+    pg.exec()
