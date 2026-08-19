c5d0d341b23f46ca012a725e6284f6b004d688e8
Valentin Carrez <valentin.carrez@rte-france.com>
2026-08-19 16:48:55 +0200

    remove MinimumSpanningTreeGraphConnectivity
    Signed-off-by: Valentin Carrez <valentin.carrez@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/graph/BridgesTest.java
+++ src/test/java/com/powsybl/openloadflow/graph/BridgesTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * Copyright (c) 2020-2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -66,12 +66,6 @@ class BridgesTest {
     @Test
     void testEvenShiloach() {
         Set<String> bridges = testBridgesOnConnectivity(lfNetwork, new EvenShiloachGraphDecrementalConnectivity<>(), "Even-Shiloach");
-        assertEquals(bridgesSetReference, bridges);
-    }
-
-    @Test
-    void testMst() {
-        Set<String> bridges = testBridgesOnConnectivity(lfNetwork, new MinimumSpanningTreeGraphConnectivity<>(), "Even-Shiloach");
         assertEquals(bridgesSetReference, bridges);
     }
 
