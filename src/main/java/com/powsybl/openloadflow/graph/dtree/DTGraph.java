60445447efcc6ad3c462e956d0d12ff4e598f12b
Valentin Carrez <valentin.carrez@rte-france.com>
2026-08-24 16:51:48 +0200

    add DTNodeWithDepth record
    Signed-off-by: Valentin Carrez <valentin.carrez@rte-france.com>

--- src/main/java/com/powsybl/openloadflow/graph/dtree/DTGraph.java
+++ src/main/java/com/powsybl/openloadflow/graph/dtree/DTGraph.java
@@ -8,7 +8,6 @@
 package com.powsybl.openloadflow.graph.dtree;
 
 import com.powsybl.openloadflow.graph.GraphModel;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.*;
 
@@ -36,7 +35,7 @@ public class DTGraph<V, E> implements GraphModel<V, E> {
         long sum = 0;
 
         for (DTNode<V, E> node : vertexToTreeNode.values()) {
-            sum += node.findRootWithDepth().getValue();
+            sum += node.findRootWithDepth().depth();
         }
 
         return sum;
@@ -67,17 +66,17 @@ public class DTGraph<V, E> implements GraphModel<V, E> {
         edges.put(e, edge);
 
         // update spanning trees
-        Pair<DTNode<V, E>, Integer> rootUdist = nodeU.findRootWithDepth();
-        Pair<DTNode<V, E>, Integer> rootVdist = nodeV.findRootWithDepth();
+        DTNodeWithDepth<V, E> rootUdepth = nodeU.findRootWithDepth();
+        DTNodeWithDepth<V, E> rootVdepth = nodeV.findRootWithDepth();
 
         boolean treeEdge;
-        if (rootUdist.getKey() == rootVdist.getKey()) {
+        if (rootUdepth.node() == rootVdepth.node()) {
             // insert non tree edge
-            treeEdge = insertNonTreeEdge(rootUdist.getKey(), nodeU, rootUdist.getValue(), nodeV, rootVdist.getValue(), edge);
+            treeEdge = insertNonTreeEdge(rootUdepth.node(), nodeU, rootUdepth.depth(), nodeV, rootVdepth.depth(), edge);
         } else {
             // insert tree edge
             treeEdge = true;
-            insertTreeEdge(rootUdist.getKey(), nodeU, rootVdist.getKey(), nodeV, edge);
+            insertTreeEdge(rootUdepth.node(), nodeU, rootVdepth.node(), nodeV, edge);
         }
         edge.setTreeEdge(treeEdge);
     }
