14a02361d8ddd33d131cecd7e83d1b7b422f52fd
Olivier Perrin <olivier.perrin@rte-france.com>
2026-09-21 17:58:45 +0200

    Cleaning some comments
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

--- src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
+++ src/test/java/com/powsybl/openloadflow/sa/OpenSecurityAnalysisTest.java
@@ -232,7 +232,7 @@ class OpenSecurityAnalysisTest extends AbstractOpenSecurityAnalysisTest {
                 limitScalings,
                 new SecurityAnalysisParameters());
 
-        // WITH LIMIT "SCALING" (All temporary limits x 1.5)
+        // WITH LIMIT SCALING (All temporary limits x 1.5)
         //
         // Line NHV1_NHV2_1 side ONE
         //     0.5' limit (x 1.5)  : 1600 A -> 2400 A
@@ -278,7 +278,7 @@ class OpenSecurityAnalysisTest extends AbstractOpenSecurityAnalysisTest {
                 limitScalings,
                 new SecurityAnalysisParameters());
 
-        // AFTER LIMIT "REDUCTION"
+        // AFTER LIMIT SCALING
         //
         // Line NHV1_NHV2_1 side ONE
         //     ---------------------------- Post-contingency state : 1008.9 A
